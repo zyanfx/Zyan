1773cf82b3c226e5870e4b01a3a75b4e95776da0
Rainbird_cp <hagen.siegel@the-rainbird.de>
2010-11-16 05:54:30 +0000

    Versionsnummer erhöht

--- Zyan.Communication/Version.cs
+++ Zyan.Communication/Version.cs
@@ -2,5 +2,5 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.9.2.0")]
-[assembly: AssemblyFileVersion("0.9.2.0")]
+[assembly: AssemblyVersion("0.9.2.1")]
+[assembly: AssemblyFileVersion("0.9.2.1")]
