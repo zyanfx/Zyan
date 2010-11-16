063dfad8243383626e122efd50c1ebe4c0fe9c80
Rainbird_cp <hagen.siegel@the-rainbird.de>
2010-11-16 05:57:44 +0000

    Wert der CREDENTIAL-Konstanten geändert

--- Zyan.Communication/Security/AuthRequestMessage.cs
+++ Zyan.Communication/Security/AuthRequestMessage.cs
@@ -15,22 +15,22 @@ namespace Zyan.Communication.Security
         /// <summary>
         /// Konstante für Anmeldeinformation Benutzername.
         /// </summary>
-        public const string CREDENTIAL_USERNAME = "UserName";
+        public const string CREDENTIAL_USERNAME = "username";
 
         /// <summary>
         /// Konstante für Anmeldeinformation Kennwort.
         /// </summary>
-        public const string CREDENTIAL_PASSWORD = "Password";
+        public const string CREDENTIAL_PASSWORD = "password";
 
         /// <summary>
         /// Konstante für Anmeldeinformation Domäne.
         /// </summary>
-        public const string CREDENTIAL_DOMAIN = "Domain";
+        public const string CREDENTIAL_DOMAIN = "domain";
 
         /// <summary>
         /// Konstante für Anmeldeinformation Windows-Sicherheitstoken.
         /// </summary>
-        public const string CREDENTIAL_WINDOWS_SECURITY_TOKEN = "WindowsSecurityToken";
+        public const string CREDENTIAL_WINDOWS_SECURITY_TOKEN = "windowssecuritytoken";
         
         /// <summary>
         /// Gibt die Anmeldeinformationen zurück, oder legt sie fest.
