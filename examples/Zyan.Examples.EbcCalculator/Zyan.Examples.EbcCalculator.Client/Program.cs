63e7146da8d0409e09a25747d54889a581e91a41
Hagen Siegel <hagen.siegel@the-rainbird.de>
2010-11-16 05:53:55 +0000

    - Bug im BasicWindowsAuthProvider behoben (Sicherheitsprüfung war nicht korrekt implementiert) - WebException beim impliztiten Logoff abgefangen (wird nur beim HTTP-Protokoll geworfen)
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/cyan;C6345

--- examples/Zyan.Examples.EbcCalculator/Zyan.Examples.EbcCalculator.Client/Program.cs
+++ examples/Zyan.Examples.EbcCalculator/Zyan.Examples.EbcCalculator.Client/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 using Zyan.Communication;
 using Zyan.Communication.Toolbox;
+using Zyan.Communication.Security;
+using Zyan.Communication.Protocols.Http;
 using System.Threading.Tasks;
 
 namespace Zyan.Examples.EbcCalculator
@@ -19,16 +22,68 @@ namespace Zyan.Examples.EbcCalculator
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            using (ZyanConnection connection = new ZyanConnection("tcp://localhost:8081/EbcCalc"))
-            {
-                CalcForm form = new CalcForm();
-                ICalculator proxy = connection.CreateProxy<ICalculator>();
+            ZyanConnection connection = LoginAndConnect();
 
-                form.Out_AddNumbers = Asynchronizer<AdditionRequest>.WireUp(new Action<AdditionRequest>(proxy.In_AddNumbers));
-                proxy.Out_SendResult = SyncContextSwitcher<decimal>.WireUp(new Action<decimal>(form.In_ReceiveResult));
-                
-                Application.Run(form);
+            if (connection != null)
+            {
+                try
+                {
+                    CalcForm form = new CalcForm();
+                    ICalculator proxy = connection.CreateProxy<ICalculator>();
+
+                    form.Out_AddNumbers = Asynchronizer<AdditionRequest>.WireUp(new Action<AdditionRequest>(proxy.In_AddNumbers));
+                    proxy.Out_SendResult = SyncContextSwitcher<decimal>.WireUp(new Action<decimal>(form.In_ReceiveResult));
+
+                    Application.Run(form);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Dispose();
+                }
             }
+        }
+
+        private static ZyanConnection LoginAndConnect()
+        {
+            HttpCustomClientProtocolSetup protocol = new HttpCustomClientProtocolSetup(true);
+
+            ZyanConnection connection = null;
+            bool success = false;
+            string message = string.Empty;
+
+            while (!success)
+            {
+                LoginForm loginForm = new LoginForm();
+
+                if (!string.IsNullOrEmpty(message))
+                    loginForm.Message = message;
+
+                DialogResult result = loginForm.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    Hashtable credentials = new Hashtable();
+                    credentials.Add(AuthRequestMessage.CREDENTIAL_USERNAME, loginForm.UserName);
+                    credentials.Add(AuthRequestMessage.CREDENTIAL_PASSWORD, loginForm.Password);
+
+                    try
+                    {
+                        connection = new ZyanConnection("http://localhost:8081/EbcCalc", protocol, credentials, false);
+                        success = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        message = ex.Message;
+                    }
+                }
+                else
+                    return null;
+            }
+            return connection;
         }
     }
 }
