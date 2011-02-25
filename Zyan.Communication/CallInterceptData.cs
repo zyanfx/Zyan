c63792b393f52ef170de7719040f1f81de629710
Rainbird_cp <hagen.siegel@the-rainbird.de>
2011-02-25 06:36:00 +0000

    [de] - Klassen für das Abfangen und Umleiten von Aufrufen erstellt (Noch nicht fertig) - TcpEx Remoting Kanal integriert   Neues Protokollsetup: TcpDuplexServerProtocolSetup, TcpDuplexClientProtocolSetup (Funktioniert momentan nur mit deaktivierter Verschlüsselung)
    [en]
    - Added classes for call interception (Call intercepion feature is not yet completed)
    - TcpEx Remoting Channel was integrated 
      New protocol setup: TcpDuplexServerProtocolSetup, TcpDuplexClientProtocolSetup (Works currently only with deactivated encryption)

---
+++ Zyan.Communication/CallInterceptData.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Zyan.Communication
+{
+    /// <summary>
+    /// Konkrete Aufrufabfangaktion.
+    /// </summary>
+    public class CallInterceptData
+    {
+        /// <summary>
+        /// Erstellt eine neue Instanz der CallInterceptData-Klasse.
+        /// </summary>
+        /// <param name="subscription">Aufrufabfangregistrierung</param>
+        /// <param name="parameters">Parameterwerte des abgefangenen Aufrufs</param>
+        public CallInterceptData(CallInterceptor subscription, object[] parameters)
+        {
+            // Felder füllen
+            Intercepted = false;
+            ReturnValue = null;
+            Subscription = subscription;
+            Parameters = parameters;
+        }
+
+        /// <summary>
+        /// Gibt zurück, ob der Aufruf abgefangen wurde, oder legt dies fest.
+        /// </summary>
+        public bool Intercepted
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gibt den zu verwendenden Rückgabewert zurück, oder legt ihn fest.
+        /// </summary>
+        public object ReturnValue
+        {
+            get;
+            set;
+        }
+
+        public CallInterceptor Subscription
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gibt ein Array der Parameterwerten zurück, mit welchen die abzufangende Methode aufgerufen wurde, oder legt sie fest.
+        /// </summary>
+        public object[] Parameters
+        {
+            get;
+            set;
+        }
+    }
+}
--- Zyan.Communication/Version.cs
+++ Zyan.Communication/Version.cs
@@ -2,5 +2,5 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.2.0.1")]
-[assembly: AssemblyFileVersion("1.2.0.1")]
+[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.0.0")]
