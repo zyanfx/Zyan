2814f6ebbe8ed3b18a1ccc9043a1ad267700ff13
Alexey Yakovlev <yallie@yandex.ru>
2011-03-12 14:13:33 +0000

    [en] Added basic LINQ support via InterLINQ integration: http://interlinq.codeplex.com
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/cyan;C9403

---
+++ examples/Zyan.Examples.Linq/Zyan.Examples.Linq.Server/SampleSource.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Zyan.InterLinq;
+
+namespace Zyan.Examples.Linq.Server
+{
+	public class SampleSource : IObjectSource
+	{
+		public IEnumerable<T> Get<T>() where T : class
+		{
+			// Get<FileInfo>() returns file list of MyDocuments folder
+			if (typeof(T) == typeof(FileInfo))
+			{
+				var folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+				foreach (var s in Directory.GetFiles(folder))
+				{
+					var fi = new FileInfo(s);
+					yield return (T)(object)fi;
+				}
+
+				yield break;
+			}
+
+			// Get<Assembly>() returns list of loaded assemblies
+			if (typeof(T) == typeof(Assembly))
+			{
+				foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+				{
+					var asm = (T)(object)assembly;
+					yield return asm;
+				}
+
+				yield break;
+			}
+
+			throw new NotSupportedException(string.Format("Type {0} is not supported", typeof(T).Name));
+		}
+	}
+}
---
+++ source/Zyan.InterLinq/ZyanClientQueryHandler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using InterLinq;
+using InterLinq.Communication;
+using Zyan.Communication;
+
+namespace Zyan.InterLinq
+{
+	/// <summary>
+	/// Client handler class managing Zyan connection to the InterLINQ Server.
+	/// </summary>
+	/// <seealso cref="ClientQueryHandler"/>
+	/// <seealso cref="InterLinqQueryHandler"/>
+	/// <seealso cref="IQueryHandler"/>
+	public class ZyanClientQueryHandler : ClientQueryHandler
+	{
+		public bool ImplicitTransactionTransfer { get; set; }
+
+		public ZyanConnection Connection { get; private set; }
+
+		/// <summary>
+		/// Creates ZyanClientQueryHandler instance.
+		/// </summary>
+		/// <param name="connection">Zyan connection.</param>
+		public ZyanClientQueryHandler(ZyanConnection connection)
+		{
+			if (connection == null)
+			{
+				throw new ArgumentNullException("connection");
+			}
+
+			Connection = connection;
+		}
+
+		/// <summary>
+		/// Creates ZyanClientQueryHandler instance.
+		/// </summary>
+		/// <param name="serverUrl">URL where the Remote Objects will be published.</param>
+		public ZyanClientQueryHandler(string serverUrl)
+		{
+			if (string.IsNullOrEmpty(serverUrl))
+			{
+				throw new ArgumentNullException("serverUrl");
+			}
+
+			Connection = new ZyanConnection(serverUrl);
+		}
+
+		/// <summary>
+		/// Connects to the server.
+		/// <see cref="InterLinqQueryHandler"/>
+		/// </summary>
+		/// <seealso cref="ClientQueryHandler.Connect"/>
+		public override void Connect()
+		{
+			if (queryRemoteHandler == null)
+			{
+				queryRemoteHandler = Connection.CreateProxy<IQueryRemoteHandler>(ImplicitTransactionTransfer);
+
+				if (queryRemoteHandler == null)
+				{
+					throw new Exception("Connection to server could no be made.");
+				}
+			}
+		}
+	}
+}
