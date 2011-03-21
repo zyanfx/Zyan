2f2096291f5b0829ef7d6a5d64d46294b5b82feb
yallie_cp <SND\yallie_cp>
2011-03-21 00:32:26 +0000

    [en] Added a few missing files for the unit-tests project

---
+++ branches/generics/UnitTests/Model/GenericWrapper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace UnitTests.Model
+{
+	class GenericWrapper : IGenericInterface
+	{
+		INonGenericInterface instance;
+
+		public GenericWrapper(INonGenericInterface remote)
+		{
+			instance = remote;
+		}
+
+		public T GetDefault<T>()
+		{
+			return (T)(object)instance.GetDefault_T(typeof(T));
+		}
+
+		public bool Equals<T>(T a, T b)
+		{
+			return instance.Equals_T(typeof(T), a, b);
+		}
+
+		public void DoSomething<A, B, C>(A a, B b, C c)
+		{
+			instance.DoSomething_A_B_C(typeof(A), typeof(B), typeof(C), a, b, c);
+		}
+
+		public B Compute<A, B>(A a, int b, string c)
+		{
+			return (B)(object)instance.Compute_A_B(typeof(A), typeof(B), a, b, c);
+		}
+
+		public string GetVersion()
+		{
+			return instance.GetVersion();
+		}
+
+		public Guid CreateGuid(DateTime time, int seed)
+		{
+			return instance.CreateGuid(time, seed);
+		}
+
+		public DateTime LastDate
+		{
+			get { return instance.LastDate; }
+			set { instance.LastDate = value; }
+		}
+
+		public string Name
+		{
+			get { return instance.Name; }
+		}
+
+		public int Priority
+		{
+			set
+			{
+				instance.Priority = value;
+			}
+		}
+
+		public event EventHandler<EventArgs> OnPrioritySet
+		{
+			add
+			{
+				instance.OnPrioritySet += value;
+			}
+			remove
+			{
+				instance.OnPrioritySet -= value;
+			}
+		}
+	}
+}
---
+++ branches/generics/UnitTests/Model/IGenericInterface.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTests.Model
+{
+	public interface IGenericInterface
+	{
+		T GetDefault<T>();
+
+		bool Equals<T>(T a, T b);
+
+		void DoSomething<A, B, C>(A a, B b, C c);
+
+		B Compute<A, B>(A a, int b, string c);
+
+		string GetVersion();
+
+		Guid CreateGuid(DateTime time, int seed);
+
+		DateTime LastDate { get; set; }
+
+		string Name { get; }
+
+		int Priority { set; }
+
+		event EventHandler<EventArgs> OnPrioritySet;
+	}
+}
