9a8d53d0fdab5f68898f56db2ae2e9fae00fd2f2
yallie_cp <SND\yallie_cp>
2011-03-25 23:41:28 +0000

    [en] Added OneWay methods support

---
+++ source/Zyan.Tests/OneWayTests.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zyan.Communication;
+using Zyan.Communication.Protocols.Ipc;
+using System.Threading;
+using System.Runtime.Remoting.Messaging;
+
+namespace Zyan.Tests
+{
+	/// <summary>
+	/// Test class for one-way methods
+	///</summary>
+	[TestClass]
+	public class OneWayTests
+	{
+		/// <summary>
+		/// Sample server interface
+		/// </summary>
+		public interface ISampleServer
+		{
+ 			void OneWayVoidMethod(Action beforeSleep, Action afterSleep);
+
+			string NonOneWayMethod(Action callback);
+
+			void NonOneWayMethod(ref int value, Action callback);
+		}
+
+		/// <summary>
+		/// Sample server implementation
+		/// </summary>
+		public class SampleServer : ISampleServer
+		{
+			[OneWay]
+			public void OneWayVoidMethod(Action beforeSleep, Action afterSleep)
+			{
+				beforeSleep();
+				Thread.Sleep(300);
+				afterSleep();
+			}
+
+			[OneWay] // OneWay attribute should be ignored because of the non-void return type
+			public string NonOneWayMethod(Action callback)
+			{
+				callback();
+				return "Non-empty string";
+			}
+
+			[OneWay] // OneWay attribute should be ignored because of the ref parameter
+			public void NonOneWayMethod(ref int value, Action callback)
+			{
+				callback();
+ 			}
+		}
+
+		public TestContext TestContext { get; set; }
+
+		static ZyanComponentHost ZyanHost { get; set; }
+
+		static ZyanConnection ZyanConnection { get; set; }
+
+		[ClassInitialize]
+		public static void StartServer(TestContext ctx)
+		{
+			var serverSetup = new IpcBinaryServerProtocolSetup("OneWayTest");
+			ZyanHost = new ZyanComponentHost("SampleServer", serverSetup);
+			ZyanHost.RegisterComponent<ISampleServer, SampleServer>();
+
+			var clientSetup = new IpcBinaryClientProtocolSetup();
+			ZyanConnection = new ZyanConnection("ipc://OneWayTest/SampleServer", clientSetup);
+		}
+
+		[ClassCleanup]
+		public static void StopServer()
+		{
+			ZyanConnection.Dispose();
+			ZyanHost.Dispose();
+		}
+
+		[TestMethod]
+		public void OneWayMethodCallShouldReturnImmediatelly()
+		{
+			var proxy = ZyanConnection.CreateProxy<ISampleServer>();
+			var mre = new ManualResetEvent(false);
+			var callbackExecuted = false;
+
+			// this should return immediatelly
+			proxy.OneWayVoidMethod(() =>
+			{
+				mre.Set();
+			},
+			() =>
+			{
+				callbackExecuted = true;
+				mre.Set();
+			});
+
+			// check if the method is still running
+			Assert.IsTrue(mre.WaitOne(300));
+			Assert.IsFalse(callbackExecuted);
+
+			// wait for the method to finish
+			mre.Reset();
+			Assert.IsTrue(mre.WaitOne(500));
+			Assert.IsTrue(callbackExecuted);
+		}
+
+		[TestMethod]
+		public void OneWayAttributeShouldBeIgnoredOnNonVoidMethodTest()
+		{
+			var proxy = ZyanConnection.CreateProxy<ISampleServer>();
+			var callbackExecuted = false;
+			var result = String.Empty;
+
+			// this method should be called synchronously
+			result = proxy.NonOneWayMethod(() =>
+			{
+				callbackExecuted = true;
+			});
+
+			// make sure method call was completed synchronously
+			Assert.IsFalse(String.IsNullOrEmpty(result));
+			Assert.IsTrue(callbackExecuted);
+		}
+
+		[TestMethod]
+		public void OneWayAttributeShouldBeIgnoredIfRefParametersArePresent()
+		{
+			var proxy = ZyanConnection.CreateProxy<ISampleServer>();
+			var callbackExecuted = false;
+			var refArg = 0;
+
+			// this method should be called synchronously
+			proxy.NonOneWayMethod(ref refArg, () =>
+			{
+				callbackExecuted = true;
+			});
+
+			// make sure method call was completed synchronously
+			Assert.IsTrue(callbackExecuted);
+		}
+	}
+}
