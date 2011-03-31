0bea923aeaedf7c98fe9347731e17d4de606c1d7
Rainbird_cp <hagen.siegel@the-rainbird.de>
2011-03-31 12:23:10 +0000

    [en] - Added class diagram for Duplex Channel core classes. - Added code documentation to Message class. [de] - Klassendiagramm für Kernklassen des Duplexkanals erstellt. - Code der Klasse Message dokumentiert.

---
+++ Zyan.Communication/Protocols/Tcp/DuplexChannel/MessageException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Zyan.Communication.Protocols.Tcp.DuplexChannel
+{
+    /// <summary>
+    /// Should be thrown when problems with sending or receiving a message occur.
+    /// </summary>
+    public class MessageException : IOException
+    {
+        /// <summary>
+        /// Creates a new instance of the MessageException class.
+        /// </summary>
+        /// <param name="msg">Error message</param>
+        /// <param name="innerException">Inner exception (can be null)</param>
+        /// <param name="connection">Affected Duplex Channel Connection</param>
+        public MessageException(string msg, Exception innerException, Connection connection)
+            : base(msg, innerException)
+        {
+            Connection = connection;
+        }
+
+        /// <summary>
+        /// Gets or sets the affected Duplex Channel Connection.
+        /// </summary>
+        public Connection Connection
+        {
+            get;
+            private set;
+        }
+    }
+}
