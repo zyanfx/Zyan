cf855bed712ee4810948cc83d711fb1d61ee28f6
Alexey Yakovlev <yallie@yandex.ru>
2011-04-27 14:38:07 +0000

    [en] Updated LINQ sample: — Added delegate-based queryable service example — Added global exception handler example — Replaced RegisterComponent<IQueryRemoteHandler>() with RegisterQueryableComponent()
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/cyan;C10785

--- examples/Zyan.Examples.Linq/Zyan.Examples.Linq.Server/Program.cs
+++ examples/Zyan.Examples.Linq/Zyan.Examples.Linq.Server/Program.cs
@@ -5,6 +5,7 @@ using InterLinq;
 using Zyan.Communication;
 using Zyan.Examples.Linq.Server.Properties;
 using Zyan.InterLinq;
+using System.Runtime.Remoting.Contexts;
 
 namespace Zyan.Examples.Linq.Server
 {
@@ -14,7 +15,14 @@ namespace Zyan.Examples.Linq.Server
 		{
 			using (var host = new ZyanComponentHost(Settings.Default.ServiceUri, Settings.Default.TcpPort))
 			{
-				host.RegisterComponent<IQueryRemoteHandler>(() => new ZyanServerQueryHandler(new SampleSource()));
+				// default queryable service
+				host.RegisterQueryableComponent(() => new SampleSource());
+
+				// named queryable service
+				host.RegisterQueryableComponent("DesktopService", t => GetDesktopData(t));
+
+				// buggy service (to test error handling)
+				host.RegisterComponent<IDynamicProperty, BuggyService>("BuggyService");
 
 				Console.WriteLine("Linq server started. Press ENTER to quit.");
 				Console.ReadLine();
@@ -22,19 +30,32 @@ namespace Zyan.Examples.Linq.Server
 		}
 
 		/// <summary>
-		/// Any method matching Func{Type, IEnumerable} delegate can be used as remote query handler, i.e:
-		/// host.RegisterComponent{IQueryRemoteHandler}(() => new ZyanServerQueryHandler(GetData));
+		/// Query handler for DesktopService
+		/// Any Func{Type, IEnumerable} or Func{Type, IQueryable} can be used as remote query handler, i.e:
+		/// host.RegisterQueryableComponent(t => GetData(t));
 		/// </summary>
-		/// <param name="t">IEnumerable element type</param>
-		static IEnumerable GetData(Type t)
+		/// <param name="t">Element type</param>
+		static IEnumerable GetDesktopData(Type t)
 		{
+			var folder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+			// query desktop files
 			if (t == typeof(FileInfo))
 			{
-				var folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 				foreach (string s in Directory.GetFiles(folder))
 				{
-					var fi = new FileInfo(s);
-					yield return fi;
+					yield return new FileInfo(s);
+				}
+
+				yield break;
+			}
+
+			// query folders
+			if (t == typeof(DirectoryInfo))
+			{
+				foreach (string s in Directory.GetDirectories(folder))
+				{
+					yield return new DirectoryInfo(s);
 				}
 
 				yield break;
