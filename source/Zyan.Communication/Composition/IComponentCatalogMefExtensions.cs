b27208f3a5b7c49ee90c2efa4deaaa5d1c714269
yallie_cp <SND\yallie_cp>
2011-06-21 09:54:44 +0000

    [en] Added unit tests for components published with InheritedExportAttribute

--- source/Zyan.Communication/Composition/IComponentCatalogMefExtensions.cs
+++ source/Zyan.Communication/Composition/IComponentCatalogMefExtensions.cs
@@ -27,7 +27,7 @@ namespace Zyan.Communication.Composition
 			// register exported components
 			foreach (var part in rootCatalog.Parts)
 			{
-				host.RegisterMefComponents(nonSharedCatalog, parentContainer, part);
+				host.RegisterComponents(nonSharedCatalog, parentContainer, part);
 			}
 		}
 
@@ -42,21 +42,21 @@ namespace Zyan.Communication.Composition
 			// register exported components
 			foreach (var part in container.Catalog.Parts)
 			{
-				host.RegisterMefComponents(nonSharedCatalog, container, part);
+				host.RegisterComponents(nonSharedCatalog, container, part);
 			}
 		}
 
 		/// <summary>
 		/// Registers exports provided by ComposablePartDefinition as components.
 		/// </summary>
-		static void RegisterMefComponents(this IComponentCatalog host, ComposablePartCatalog childCatalog, CompositionContainer parentContainer, ComposablePartDefinition partDefinition)
+		static void RegisterComponents(this IComponentCatalog host, ComposablePartCatalog childCatalog, CompositionContainer parentContainer, ComposablePartDefinition partDefinition)
 		{
 			foreach (var export in partDefinition.ExportDefinitions.Where(def => def.IsZyanComponent()))
 			{
 				var type = export.Metadata[ZyanComponentAttribute.ComponentInterfaceKeyName] as Type;
 				if (type != null)
 				{
-					host.RegisterMefComponent(childCatalog, parentContainer, type, export.ContractName);
+					host.RegisterComponent(childCatalog, parentContainer, type, export.ContractName);
 				}
 			}
 		}
@@ -64,13 +64,13 @@ namespace Zyan.Communication.Composition
 		/// <summary>
 		/// MethodInfo for the following method (used to create generic method delegates)
 		/// </summary>
-		static MethodInfo RegisterMefComponentMethodInfo = typeof(IComponentCatalogMefExtensions).GetMethod("RegisterMefComponent", BindingFlags.Static | BindingFlags.NonPublic,
+		static MethodInfo RegisterComponentMethodInfo = typeof(IComponentCatalogMefExtensions).GetMethod("RegisterComponent", BindingFlags.Static | BindingFlags.NonPublic,
 			null, new[] { typeof(IComponentCatalog), typeof(ComposablePartCatalog), typeof(CompositionContainer), typeof(string) }, null);
 
 		/// <summary>
 		/// Registers component of type T. Component instance is provided by MEF container.
 		/// </summary>
-		static void RegisterMefComponent<T>(this IComponentCatalog host, ComposablePartCatalog childCatalog, CompositionContainer parentContainer, string contractName = null)
+		static void RegisterComponent<T>(this IComponentCatalog host, ComposablePartCatalog childCatalog, CompositionContainer parentContainer, string contractName = null)
 		{
 			// component -> owning container
 			var containers = new ConcurrentDictionary<object, CompositionContainer>();
@@ -104,9 +104,9 @@ namespace Zyan.Communication.Composition
 		/// <summary>
 		/// Registers component of the given type. Component instance is provided by MEF container.
 		/// </summary>
-		static void RegisterMefComponent(this IComponentCatalog host, ComposablePartCatalog childCatalog, CompositionContainer parentContainer, Type type, string contractName = null)
+		static void RegisterComponent(this IComponentCatalog host, ComposablePartCatalog childCatalog, CompositionContainer parentContainer, Type type, string contractName = null)
 		{
-			var method = RegisterMefComponentMethodInfo.MakeGenericMethod(type);
+			var method = RegisterComponentMethodInfo.MakeGenericMethod(type);
 			var registerComponent = method.CreateDelegate<ComponentRegistrationDelegate>();
 			registerComponent(host, childCatalog, parentContainer, contractName);
 		}
