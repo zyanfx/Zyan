d084fae3a65dc4c385addd7bd1f45e8c39c8aa22
yallie_cp <SND\yallie_cp>
2011-06-24 23:50:08 +0000

    [en] Duck typing: registration-time checks, invocation-time checks

---
+++ source/Zyan.Communication/Toolbox/ExtensionsFx3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zyan.Communication.Toolbox
+{
+	/// <summary>
+	/// Extension methods missing in .NET Framework 3.5
+	/// </summary>
+	public static class ExtensionsFx3
+	{
+		/// <summary>
+		/// Merges two sequences by using specified selector.
+		/// </summary>
+		/// <typeparam name="TFirst">First sequence element type.</typeparam>
+		/// <typeparam name="TSecond">Second sequence element type.</typeparam>
+		/// <typeparam name="TResult">Resulting sequence element type.</typeparam>
+		/// <param name="first">First sequence.</param>
+		/// <param name="second">Second sequence.</param>
+		/// <param name="resultSelector">Selector function.</param>
+		/// <returns></returns>
+		public static IEnumerable<TResult> Zip<TFirst, TSecond, TResult>(
+			this IEnumerable<TFirst> first,
+			IEnumerable<TSecond> second,
+			Func<TFirst, TSecond, TResult> resultSelector)
+		{
+			if (first == null)
+				throw new ArgumentNullException("first");
+
+			if (second == null)
+				throw new ArgumentNullException("second");
+
+			if (resultSelector == null)
+				throw new ArgumentNullException("resultSelector");
+
+			using (var e1 = first.GetEnumerator())
+			using (var e2 = second.GetEnumerator())
+			{
+				while (e1.MoveNext() && e2.MoveNext())
+					yield return resultSelector(e1.Current, e2.Current);
+			}
+		}
+	}
+}
---
+++ source/Zyan.Communication/Toolbox/TypeComparer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Zyan.Communication.Toolbox
+{
+	/// <summary>
+	/// Helper class for duck typing support.
+	/// </summary>
+	/// <typeparam name="I">Interface type</typeparam>
+	/// <typeparam name="T">Component type</typeparam>
+	internal class TypeComparer<I, T>
+	{
+		public void Validate()
+		{
+			if (!MatchAllMethods())
+			{
+				var msg = String.Format(LanguageResource.MissingMethodException_MethodNotFound, MissingMethodSignature);
+				throw new MissingMethodException(msg);
+			}
+		}
+
+		string MissingMethodSignature { get; set; }
+
+		private bool MatchAllMethods()
+		{
+			var interfaceType = typeof(I);
+			var componentType = typeof(T);
+
+			// check if componentType actually implements interfaceType
+			if (interfaceType.IsAssignableFrom(componentType))
+			{
+				return true;
+			}
+
+			// check if every interface method is implemented by the component type
+			var interfaceMethods = interfaceType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+			var componentMethods = componentType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+			return interfaceMethods.All(im =>
+			{
+				var match = componentMethods.Any(cm => MethodsAreEqual(im, cm));
+				if (!match)
+				{ 
+					var paramTypes = im.GetParameters().Select(pi => pi.ParameterType).ToArray();
+					MissingMethodSignature = MessageHelpers.GetMethodSignature(componentType, im.Name, paramTypes);
+				}
+
+				return match;
+			});
+		}
+
+		private bool MethodsAreEqual(MethodInfo im, MethodInfo cm)
+		{
+			// compare name and return type
+			if (im.Name != cm.Name ||
+				im.ReturnType != cm.ReturnType ||
+				im.IsGenericMethod != cm.IsGenericMethod ||
+				im.IsGenericMethodDefinition != cm.IsGenericMethodDefinition)
+				return false;
+
+			// compare parameter count
+			var imParams = im.GetParameters();
+			var cmParams = cm.GetParameters();
+			if (imParams.Length != cmParams.Length)
+				return false;
+
+			// compare parameter types
+			return imParams.Zip(cmParams, (ip, cp) => ip.ParameterType == cp.ParameterType).All(p => p);
+		}
+	}
+}
