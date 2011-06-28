fa1878bb58cc6cf1131563f1beff82fc2f8dd785
Alexey Yakovlev <yallie@yandex.ru>
2011-06-28 12:46:10 +0000

    [en] Added InterLinq expression deserialization: http://interlinq.codeplex.com/discussions/60896
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/cyan;C12147

---
+++ libraries/InterLinq/Source/InterLinq.UnitTests/SerializationTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq.Expressions;
+using InterLinq.Expressions;
+
+namespace InterLinq.UnitTests
+{
+	/// <summary>
+	/// Expression serialization tests
+	/// </summary>
+	[TestClass]
+	public class SerializationTest
+	{
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext { get; set; }
+
+		/// <summary>
+		/// Simple expression
+		/// </summary>
+		[TestMethod]
+		public void TestSimpleSerialization()
+		{
+			// http://interlinq.codeplex.com/discussions/60896
+			Expression<Func<Guid, bool>> expression = (guid) => guid != Guid.Empty;
+
+			var sx = expression.MakeSerializable();
+			var dx = sx.Deserialize();
+
+			Assert.AreEqual(expression.ToString(), dx.ToString());
+		}
+
+		/// <summary>
+		/// A bit more complex expression
+		/// </summary>
+		[TestMethod]
+		public void TestComplexSerialization()
+		{
+			Expression<Func<Expression, bool>> expression = t => t.Type.FullName.ToLower().EndsWith("e");
+			var sx = expression.MakeSerializable();
+			var dx = sx.Deserialize();
+
+			Assert.AreEqual(expression.ToString(), dx.ToString());
+		}
+	}
+}
--- libraries/InterLinq/Source/InterLinq/Expressions/ExpressionConversionHelper.cs
+++ libraries/InterLinq/Source/InterLinq/Expressions/ExpressionConversionHelper.cs
@@ -2,78 +2,92 @@
 using System.Collections.ObjectModel;
 using System.Linq.Expressions;
 using InterLinq.Expressions.Helpers;
+using InterLinq.Communication;
 
 namespace InterLinq.Expressions
 {
-    /// <summary>
-    /// Static helper class providing several C# 3.0 Extension Methods.
-    /// </summary>
-    /// <seealso cref="SerializableExpression"/>
-    /// <seealso cref="Expression"/>
-    /// <seealso cref="IEnumerable"/>
-    public static class ExpressionConversionHelper
-    {
+	/// <summary>
+	/// Static helper class providing several C# 3.0 Extension Methods.
+	/// </summary>
+	/// <seealso cref="SerializableExpression"/>
+	/// <seealso cref="Expression"/>
+	/// <seealso cref="IEnumerable"/>
+	public static class ExpressionConversionHelper
+	{
+		/// <summary>
+		/// Extension Method for <see cref="Expression"/>.
+		/// Converts an <see cref="Expression"/> to a <see cref="SerializableExpression"/>.
+		/// </summary>
+		/// <param name="exp">Extended class instance.</param>
+		/// <returns>Returns the converted <see cref="SerializableExpression"/>.</returns>
+		public static SerializableExpression MakeSerializable(this Expression exp)
+		{
+			return new ExpressionConverter(exp).Visit() as SerializableExpression;
+		}
 
-        /// <summary>
-        /// Extension Method for <see cref="Expression"/>.
-        /// Converts an <see cref="Expression"/> to a <see cref="SerializableExpression"/>.
-        /// </summary>
-        /// <param name="exp">Extended class instance.</param>
-        /// <returns>Returns the converted <see cref="SerializableExpression"/>.</returns>
-        public static SerializableExpression MakeSerializable(this Expression exp)
-        {
-            return new ExpressionConverter(exp).Visit() as SerializableExpression;
-        }
+		/// <summary>
+		/// Extension Method for <see cref="Expression"/>.
+		/// Converts an <see cref="Expression"/> to a <see cref="SerializableExpression"/>.
+		/// </summary>
+		/// <param name="exp">Extended class instance.</param>
+		/// <param name="expConverter"><see cref="ExpressionConverter"/> instance.</param>
+		/// <returns>Returns the converted <see cref="SerializableExpression"/>.</returns>
+		public static SerializableExpression MakeSerializable(this Expression exp, ExpressionConverter expConverter)
+		{
+			return expConverter.Convert(exp);
+		}
 
-        /// <summary>
-        /// Extension Method for <see cref="Expression"/>.
-        /// Converts an <see cref="Expression"/> to a <see cref="SerializableExpression"/>.
-        /// </summary>
-        /// <param name="exp">Extended class instance.</param>
-        /// <param name="expConverter"><see cref="ExpressionConverter"/> instance.</param>
-        /// <returns>Returns the converted <see cref="SerializableExpression"/>.</returns>
-        public static SerializableExpression MakeSerializable(this Expression exp, ExpressionConverter expConverter)
-        {
-            return expConverter.Convert(exp);
-        }
+		/// <summary>
+		/// Extension Method for <see cref="Expression"/>.
+		/// Converts an <see cref="Expression"/> to a <typeparamref name="T"/>.
+		/// </summary>
+		/// <typeparam name="T">Return type (must be subclass of <see cref="SerializableExpression"/>.</typeparam>
+		/// <param name="exp">Extended class instance.</param>
+		/// <param name="expConverter"><see cref="ExpressionConverter"/> instance.</param>
+		/// <returns>Returns the converted <typeparamref name="T"/>.</returns>
+		public static T MakeSerializable<T>(this Expression exp, ExpressionConverter expConverter) where T : SerializableExpression
+		{
+			return expConverter.Convert<T>(exp);
+		}
 
-        /// <summary>
-        /// Extension Method for <see cref="Expression"/>.
-        /// Converts an <see cref="Expression"/> to a <typeparamref name="T"/>.
-        /// </summary>
-        /// <typeparam name="T">Return type (must be subclass of <see cref="SerializableExpression"/>.</typeparam>
-        /// <param name="exp">Extended class instance.</param>
-        /// <param name="expConverter"><see cref="ExpressionConverter"/> instance.</param>
-        /// <returns>Returns the converted <typeparamref name="T"/>.</returns>
-        public static T MakeSerializable<T>(this Expression exp, ExpressionConverter expConverter) where T : SerializableExpression
-        {
-            return expConverter.Convert<T>(exp);
-        }
+		/// <summary>
+		/// Extension Method for <see cref="IEnumerable"/>.
+		/// Converts an <see cref="IEnumerable"/> to a <see cref="ReadOnlyCollection{T}"/>.
+		/// </summary>
+		/// <typeparam name="T">Return type (must be subclass of <see cref="SerializableExpression"/>.</typeparam>
+		/// <param name="exp">Extended class instance.</param>
+		/// <param name="expConverter"><see cref="ExpressionConverter"/> instance.</param>
+		/// <returns>Returns the converted <see cref="ReadOnlyCollection{T}"/>.</returns>
+		public static ReadOnlyCollection<T> MakeSerializableCollection<T>(this IEnumerable exp, ExpressionConverter expConverter) where T : SerializableExpression
+		{
+			return expConverter.ConvertCollection<T>(exp);
+		}
 
-        /// <summary>
-        /// Extension Method for <see cref="IEnumerable"/>.
-        /// Converts an <see cref="IEnumerable"/> to a <see cref="ReadOnlyCollection{T}"/>.
-        /// </summary>
-        /// <typeparam name="T">Return type (must be subclass of <see cref="SerializableExpression"/>.</typeparam>
-        /// <param name="exp">Extended class instance.</param>
-        /// <param name="expConverter"><see cref="ExpressionConverter"/> instance.</param>
-        /// <returns>Returns the converted <see cref="ReadOnlyCollection{T}"/>.</returns>
-        public static ReadOnlyCollection<T> MakeSerializableCollection<T>(this IEnumerable exp, ExpressionConverter expConverter) where T : SerializableExpression
-        {
-            return expConverter.ConvertCollection<T>(exp);
-        }
+		/// <summary>
+		/// Extension Method for <see cref="IEnumerable"/>.
+		/// Returns the result of the executed <see cref="SerializableExpression"/>.
+		/// </summary>
+		/// <param name="exp">Extended class instance.</param>
+		/// <param name="linqHandler"><see cref="IQueryHandler"/>.</param>
+		/// <returns>Returns the result of the executed <see cref="SerializableExpression"/>.</returns>
+		public static object Convert(this SerializableExpression exp, IQueryHandler linqHandler)
+		{
+			return new SerializableExpressionConverter(exp, linqHandler).Visit();
+		}
 
-        /// <summary>
-        /// Extension Method for <see cref="IEnumerable"/>.
-        /// Returns the result of the executed <see cref="SerializableExpression"/>.
-        /// </summary>
-        /// <param name="exp">Extended class instance.</param>
-        /// <param name="linqHandler"><see cref="IQueryHandler"/>.</param>
-        /// <returns>Returns the result of the executed <see cref="SerializableExpression"/>.</returns>
-        public static object Convert(this SerializableExpression exp, IQueryHandler linqHandler)
-        {
-            return new SerializableExpressionConverter(exp, linqHandler).Visit();
-        }
+		/// <summary>
+		/// Extension method for <see cref="SerializableExpression"/>.
+		/// Returns <see cref="Expression"/>.
+		/// </summary>
+		/// <param name="exp">Serializable expression to convert</param>
+		/// <returns>Returns <see cref="Expression"/>.</returns>
+		public static Expression Deserialize(this SerializableExpression exp)
+		{
+			// http://interlinq.codeplex.com/discussions/60896
+			var fakeHandler = new ClientQueryHandler();
+			var visitor = new SerializableExpressionConverter(exp, fakeHandler);
 
-    }
+			return visitor.Visit(exp);
+		}
+	}
 }
