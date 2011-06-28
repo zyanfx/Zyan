d98b4365ab59ea8799a91c4bce88e9b42521c30e
Alexey Yakovlev <yallie@users.noreply.github.com>
2011-06-28 16:13:42 +0000

    [en] Moved files from Zyan.InterLinq to Zyan.Communication.

---
+++ Zyan.Communication/InterLinq/IEntitySource.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Zyan.InterLinq
+{
+	/// <summary>
+	/// Interface required for the built-in Linq to entities support
+	/// </summary>
+	public interface IEntitySource : IBaseSource
+	{
+		IQueryable<T> Get<T>() where T : class;
+	}
+}
---
+++ Zyan.Communication/InterLinq/IObjectSource.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Zyan.InterLinq
+{
+	/// <summary>
+	/// Interface required for the built-in Linq to objects support
+	/// </summary>
+	public interface IObjectSource : IBaseSource
+	{
+		IEnumerable<T> Get<T>() where T : class;
+	}
+}
