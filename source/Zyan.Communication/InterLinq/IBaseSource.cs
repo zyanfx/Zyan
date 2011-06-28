f6c3c480700bcd1244b40114eb40e2774af9a652
yallie_cp <SND\yallie_cp>
2011-06-28 16:13:42 +0000

    [en] Moved files from Zyan.InterLinq to Zyan.Communication.

---
+++ source/Zyan.Communication/InterLinq/IBaseSource.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Zyan.InterLinq
+{
+	/// <summary>
+	/// Base interface the built-in Linq support
+	/// </summary>
+	public interface IBaseSource
+	{
+	}
+}
