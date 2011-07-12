b998367b7b01d552c7c7be4f45403abbd4d783e3
Alexey Yakovlev <yallie@users.noreply.github.com>
2011-07-12 11:05:23 +0000

    [en] Cleanup.

--- IntegrationTest_DistributedEvents/TcpDuplexTest.cs
+++ IntegrationTest_DistributedEvents/TcpDuplexTest.cs
@@ -1,160 +1,158 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using Zyan.Communication;
 using Zyan.Communication.Protocols.Tcp;
-using System.Threading;
 
 namespace IntegrationTest_DistributedEvents
 {
-    public static class TcpDuplexTest
-    {
-        private static ZyanConnection _connectionDuplex;
-        private static IEventComponentSingleton _proxySingletonDuplex;
-        private static IEventComponentSingleCall _proxySingleCallDuplex;
-        private static ICallbackComponentSingleton _proxyCallbackSingletonDuplex;
-        private static ICallbackComponentSingleCall _proxyCallbackSingleCallDuplex;
-        private static IRequestResponseCallbackSingleCall _proxyRequestResponseSingleCallDuplex;
-        private static ITimerTriggeredEvent _proxyTimerTriggeredEvent;
+	public static class TcpDuplexTest
+	{
+		private static ZyanConnection _connectionDuplex;
+		private static IEventComponentSingleton _proxySingletonDuplex;
+		private static IEventComponentSingleCall _proxySingleCallDuplex;
+		private static ICallbackComponentSingleton _proxyCallbackSingletonDuplex;
+		private static ICallbackComponentSingleCall _proxyCallbackSingleCallDuplex;
+		private static IRequestResponseCallbackSingleCall _proxyRequestResponseSingleCallDuplex;
+		private static ITimerTriggeredEvent _proxyTimerTriggeredEvent;
 
-        private static int _firedCountSingletonDuplex = 0;
-        private static int _firedCountSingleCallDuplex = 0;
-        private static int _registrationsSingletonDuplex = 0;
-        private static int _registrationsSingleCallDuplex = 0;
-        private static int _callbackCountSingletonDuplex = 0;
-        private static int _callbackCountSingleCallDuplex = 0;
-        private static int _firedTimerTriggeredEvent = 0;
+		private static int _firedCountSingletonDuplex = 0;
+		private static int _firedCountSingleCallDuplex = 0;
+		private static int _registrationsSingletonDuplex = 0;
+		private static int _registrationsSingleCallDuplex = 0;
+		private static int _callbackCountSingletonDuplex = 0;
+		private static int _callbackCountSingleCallDuplex = 0;
+		private static int _firedTimerTriggeredEvent = 0;
 
-        private static void RegisterEventsDuplex()
-        {
-            _proxySingletonDuplex.ServerEvent += new Action<string>(_proxySingletonDuplex_ServerEvent);
-            _registrationsSingletonDuplex++;
-            _proxySingleCallDuplex.ServerEvent += new Action<string>(_proxySingleCallDuplex_ServerEvent);
-            _registrationsSingleCallDuplex++;
+		private static void RegisterEventsDuplex()
+		{
+			_proxySingletonDuplex.ServerEvent += new Action<string>(_proxySingletonDuplex_ServerEvent);
+			_registrationsSingletonDuplex++;
+			_proxySingleCallDuplex.ServerEvent += new Action<string>(_proxySingleCallDuplex_ServerEvent);
+			_registrationsSingleCallDuplex++;
 
-            _proxyTimerTriggeredEvent.Tick += new Action<DateTime>(_proxyTimerTriggeredEvent_Tick);            
-        }
+			_proxyTimerTriggeredEvent.Tick += new Action<DateTime>(_proxyTimerTriggeredEvent_Tick);
+		}
 
-        static void _proxyTimerTriggeredEvent_Tick(DateTime obj)
-        {
-            _firedTimerTriggeredEvent++;
-        }
+		static void _proxyTimerTriggeredEvent_Tick(DateTime obj)
+		{
+			_firedTimerTriggeredEvent++;
+		}
 
-        private static void UnregisterEventsDuplex()
-        {
-            _proxySingletonDuplex.ServerEvent -= new Action<string>(_proxySingletonDuplex_ServerEvent);
-            _registrationsSingletonDuplex--;
-            _proxySingleCallDuplex.ServerEvent -= new Action<string>(_proxySingleCallDuplex_ServerEvent);
-            _registrationsSingleCallDuplex--;
+		private static void UnregisterEventsDuplex()
+		{
+			_proxySingletonDuplex.ServerEvent -= new Action<string>(_proxySingletonDuplex_ServerEvent);
+			_registrationsSingletonDuplex--;
+			_proxySingleCallDuplex.ServerEvent -= new Action<string>(_proxySingleCallDuplex_ServerEvent);
+			_registrationsSingleCallDuplex--;
 
-            //_proxyTimerTriggeredEvent.Tick += new Action<DateTime>(_proxyTimerTriggeredEvent_Tick);
-        }
+			//_proxyTimerTriggeredEvent.Tick += new Action<DateTime>(_proxyTimerTriggeredEvent_Tick);
+		}
 
-        private static void _proxySingletonDuplex_ServerEvent(string obj)
-        {
-            _firedCountSingletonDuplex++;
-        }
+		private static void _proxySingletonDuplex_ServerEvent(string obj)
+		{
+			_firedCountSingletonDuplex++;
+		}
 
-        private static void _proxySingleCallDuplex_ServerEvent(string obj)
-        {
-            _firedCountSingleCallDuplex++;
-        }
+		private static void _proxySingleCallDuplex_ServerEvent(string obj)
+		{
+			_firedCountSingleCallDuplex++;
+		}
 
-        private static void CallBackSingletonDuplex(string text)
-        {
-            _callbackCountSingletonDuplex++;
-        }
+		private static void CallBackSingletonDuplex(string text)
+		{
+			_callbackCountSingletonDuplex++;
+		}
 
-        private static void CallBackSingleCallDuplex(string text)
-        {
-            _callbackCountSingleCallDuplex++;
-        }
+		private static void CallBackSingleCallDuplex(string text)
+		{
+			_callbackCountSingleCallDuplex++;
+		}
 
-        public static int RunTest()
-        {
-            // Duplex TCP Channel
-            TcpDuplexClientProtocolSetup protocol = new TcpDuplexClientProtocolSetup(true);
-            _connectionDuplex = new ZyanConnection("tcpex://localhost:8084/DuplexEventTest", protocol);
+		public static int RunTest()
+		{
+			// Duplex TCP Channel
+			TcpDuplexClientProtocolSetup protocol = new TcpDuplexClientProtocolSetup(true);
+			_connectionDuplex = new ZyanConnection("tcpex://localhost:8084/DuplexEventTest", protocol);
 
-            _proxySingletonDuplex = _connectionDuplex.CreateProxy<IEventComponentSingleton>();
-            _proxySingleCallDuplex = _connectionDuplex.CreateProxy<IEventComponentSingleCall>();
-            _proxyCallbackSingletonDuplex = _connectionDuplex.CreateProxy<ICallbackComponentSingleton>();
-            _proxyCallbackSingleCallDuplex = _connectionDuplex.CreateProxy<ICallbackComponentSingleCall>();
-            _proxyRequestResponseSingleCallDuplex = _connectionDuplex.CreateProxy<IRequestResponseCallbackSingleCall>();
-            _proxyTimerTriggeredEvent = _connectionDuplex.CreateProxy<ITimerTriggeredEvent>();
-            
-            _proxyTimerTriggeredEvent.StartTimer();
+			_proxySingletonDuplex = _connectionDuplex.CreateProxy<IEventComponentSingleton>();
+			_proxySingleCallDuplex = _connectionDuplex.CreateProxy<IEventComponentSingleCall>();
+			_proxyCallbackSingletonDuplex = _connectionDuplex.CreateProxy<ICallbackComponentSingleton>();
+			_proxyCallbackSingleCallDuplex = _connectionDuplex.CreateProxy<ICallbackComponentSingleCall>();
+			_proxyRequestResponseSingleCallDuplex = _connectionDuplex.CreateProxy<IRequestResponseCallbackSingleCall>();
+			_proxyTimerTriggeredEvent = _connectionDuplex.CreateProxy<ITimerTriggeredEvent>();
 
-             List<int> stepsDone = new List<int>();
+			_proxyTimerTriggeredEvent.StartTimer();
 
-            _proxyCallbackSingletonDuplex.Out_Callback = CallBackSingletonDuplex;
-            _proxyCallbackSingleCallDuplex.Out_Callback = CallBackSingleCallDuplex;
+			List<int> stepsDone = new List<int>();
 
-            _proxyCallbackSingletonDuplex.DoSomething();
-            if (_callbackCountSingletonDuplex == 1)
-            {
-                stepsDone.Add(1);
-                Console.WriteLine("[TCP Duplex] Singleton Callback Test passed.");
-            }
-            _proxyCallbackSingleCallDuplex.DoSomething();
-            if (_callbackCountSingleCallDuplex == 1)
-            {                
-                stepsDone.Add(2);
-                Console.WriteLine("[TCP Duplex] SingleCall Callback Test passed.");
-            }
+			_proxyCallbackSingletonDuplex.Out_Callback = CallBackSingletonDuplex;
+			_proxyCallbackSingleCallDuplex.Out_Callback = CallBackSingleCallDuplex;
 
-            RegisterEventsDuplex();
+			_proxyCallbackSingletonDuplex.DoSomething();
+			if (_callbackCountSingletonDuplex == 1)
+			{
+				stepsDone.Add(1);
+				Console.WriteLine("[TCP Duplex] Singleton Callback Test passed.");
+			}
+			_proxyCallbackSingleCallDuplex.DoSomething();
+			if (_callbackCountSingleCallDuplex == 1)
+			{
+				stepsDone.Add(2);
+				Console.WriteLine("[TCP Duplex] SingleCall Callback Test passed.");
+			}
 
-            if (_registrationsSingletonDuplex == _proxySingletonDuplex.Registrations)
-                stepsDone.Add(3);
-            if (_registrationsSingleCallDuplex == _proxySingleCallDuplex.Registrations)
-                stepsDone.Add(4);
+			RegisterEventsDuplex();
 
-            _proxySingletonDuplex.TriggerEvent();
-            if (_firedCountSingletonDuplex == 1)
-            {            
-                stepsDone.Add(5);
-                Console.WriteLine("[TCP Duplex] Singleton Event Test passed.");
-            }
+			if (_registrationsSingletonDuplex == _proxySingletonDuplex.Registrations)
+				stepsDone.Add(3);
+			if (_registrationsSingleCallDuplex == _proxySingleCallDuplex.Registrations)
+				stepsDone.Add(4);
 
-            _proxySingleCallDuplex.TriggerEvent();
-            if (_firedCountSingleCallDuplex == 1)
-            {             
-                stepsDone.Add(6);
-                Console.WriteLine("[TCP Duplex] SingleCall Event Test passed.");
-            }
+			_proxySingletonDuplex.TriggerEvent();
+			if (_firedCountSingletonDuplex == 1)
+			{
+				stepsDone.Add(5);
+				Console.WriteLine("[TCP Duplex] Singleton Event Test passed.");
+			}
 
-            Thread.Sleep(1000);
+			_proxySingleCallDuplex.TriggerEvent();
+			if (_firedCountSingleCallDuplex == 1)
+			{
+				stepsDone.Add(6);
+				Console.WriteLine("[TCP Duplex] SingleCall Event Test passed.");
+			}
 
-            if (_firedTimerTriggeredEvent > 1)
-            {                
-                stepsDone.Add(7);
-                Console.WriteLine("[TCP Duplex] Timer triggered Event Test passed.");
-            }
+			Thread.Sleep(1000);
 
-            UnregisterEventsDuplex();
+			if (_firedTimerTriggeredEvent > 1)
+			{
+				stepsDone.Add(7);
+				Console.WriteLine("[TCP Duplex] Timer triggered Event Test passed.");
+			}
 
-            if (_registrationsSingletonDuplex == _proxySingletonDuplex.Registrations)
-                stepsDone.Add(8);
-            if (_registrationsSingleCallDuplex == _proxySingleCallDuplex.Registrations)
-                stepsDone.Add(9);
+			UnregisterEventsDuplex();
 
-            RequestResponseResult requestResponseResult = new RequestResponseResult("TCP Duplex");
+			if (_registrationsSingletonDuplex == _proxySingletonDuplex.Registrations)
+				stepsDone.Add(8);
+			if (_registrationsSingleCallDuplex == _proxySingleCallDuplex.Registrations)
+				stepsDone.Add(9);
 
-            _proxyRequestResponseSingleCallDuplex.DoRequestResponse("Success", requestResponseResult.ReceiveResponseSingleCall);
+			RequestResponseResult requestResponseResult = new RequestResponseResult("TCP Duplex");
 
-            Thread.Sleep(1000);
+			_proxyRequestResponseSingleCallDuplex.DoRequestResponse("Success", requestResponseResult.ReceiveResponseSingleCall);
 
-            if (requestResponseResult.Count == 1)
-                stepsDone.Add(10);
+			Thread.Sleep(1000);
 
-            _connectionDuplex.Dispose();
+			if (requestResponseResult.Count == 1)
+				stepsDone.Add(10);
 
-            if (stepsDone.Count == 10)
-                return 0;
-            else
-                return 1;
-        }
-    }
+			_connectionDuplex.Dispose();
+
+			if (stepsDone.Count == 10)
+				return 0;
+			else
+				return 1;
+		}
+	}
 }
--- IntegrationTest_DistributedEvents/TestAppCode.cs
+++ IntegrationTest_DistributedEvents/TestAppCode.cs
@@ -1,243 +1,237 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Timers;
 using Zyan.Communication;
-using Zyan.Communication.Protocols;
 using Zyan.Communication.Protocols.Tcp;
 using Zyan.Communication.Security;
-using System.Timers;
 
 namespace IntegrationTest_DistributedEvents
 {
-    public interface ICallbackComponentSingleton
-    {
-        Action<string> Out_Callback { get; set; }
+	public interface ICallbackComponentSingleton
+	{
+		Action<string> Out_Callback { get; set; }
 
-        void DoSomething();
-    }
+		void DoSomething();
+	}
 
-    public class CallbackComponentSingleton : ICallbackComponentSingleton
-    {
-        public Action<string> Out_Callback
-        {
-            get;
-            set;
-        }
+	public class CallbackComponentSingleton : ICallbackComponentSingleton
+	{
+		public Action<string> Out_Callback
+		{
+			get;
+			set;
+		}
 
-        public void DoSomething()
-        {
-            if (Out_Callback != null)
-                Out_Callback(DateTime.Now.ToString());
-        }
-    }
+		public void DoSomething()
+		{
+			if (Out_Callback != null)
+				Out_Callback(DateTime.Now.ToString());
+		}
+	}
 
-    public interface ICallbackComponentSingleCall
-    {
-        Action<string> Out_Callback { get; set; }
+	public interface ICallbackComponentSingleCall
+	{
+		Action<string> Out_Callback { get; set; }
 
-        void DoSomething();
-    }
+		void DoSomething();
+	}
 
-    public class CallbackComponentSingleCall : ICallbackComponentSingleCall
-    {
-        public Action<string> Out_Callback
-        {
-            get;
-            set;
-        }
+	public class CallbackComponentSingleCall : ICallbackComponentSingleCall
+	{
+		public Action<string> Out_Callback
+		{
+			get;
+			set;
+		}
 
-        public void DoSomething()
-        {
-            if (Out_Callback != null)
-                Out_Callback(DateTime.Now.ToString());
-        }
-    }
+		public void DoSomething()
+		{
+			if (Out_Callback != null)
+				Out_Callback(DateTime.Now.ToString());
+		}
+	}
 
-    public interface IEventComponentSingleton
-    {
-        event Action<string> ServerEvent;
+	public interface IEventComponentSingleton
+	{
+		event Action<string> ServerEvent;
 
-        void TriggerEvent();
+		void TriggerEvent();
 
-        int Fired { get; }
+		int Fired { get; }
 
-        int Registrations { get; }
-    }
+		int Registrations { get; }
+	}
 
-    public class EventComponentSingleton : IEventComponentSingleton
-    {
-        public event Action<string> ServerEvent;
+	public class EventComponentSingleton : IEventComponentSingleton
+	{
+		public event Action<string> ServerEvent;
 
-        int _firedCount = 0;
+		int _firedCount = 0;
 
-        public void TriggerEvent()
-        {
-            if (ServerEvent != null)
-            {
-                _firedCount++;
-                ServerEvent(DateTime.Now.ToString());
-            }
-        }
+		public void TriggerEvent()
+		{
+			if (ServerEvent != null)
+			{
+				_firedCount++;
+				ServerEvent(DateTime.Now.ToString());
+			}
+		}
 
-        public int Fired 
-        {
-            get { return _firedCount; } 
-        }
+		public int Fired 
+		{
+			get { return _firedCount; } 
+		}
 
-        public int Registrations 
-        {
-            get 
-            {
-                if (ServerEvent != null)
-                    return ServerEvent.GetInvocationList().Length;
-                else
-                    return 0;
-            }
-        }
-    }
+		public int Registrations 
+		{
+			get 
+			{
+				if (ServerEvent != null)
+					return ServerEvent.GetInvocationList().Length;
+				else
+					return 0;
+			}
+		}
+	}
 
-    public interface IEventComponentSingleCall
-    {
-        event Action<string> ServerEvent;
+	public interface IEventComponentSingleCall
+	{
+		event Action<string> ServerEvent;
 
-        void TriggerEvent();
-        
-        int Registrations { get; }
-    }
+		void TriggerEvent();
+		
+		int Registrations { get; }
+	}
 
-    public class EventComponentSingleCall : IEventComponentSingleCall
-    {
-        public event Action<string> ServerEvent;
-        
-        public void TriggerEvent()
-        {
-            if (ServerEvent != null)
-            {
-                ServerEvent(DateTime.Now.ToString());
-            }
-        }
+	public class EventComponentSingleCall : IEventComponentSingleCall
+	{
+		public event Action<string> ServerEvent;
+		
+		public void TriggerEvent()
+		{
+			if (ServerEvent != null)
+			{
+				ServerEvent(DateTime.Now.ToString());
+			}
+		}
 
-        public int Registrations
-        {
-            get
-            {
-                if (ServerEvent != null)
-                    return ServerEvent.GetInvocationList().Length;
-                else
-                    return 0;
-            }
-        }
-    }
-    
-    public interface IRequestResponseCallbackSingleCall
-    {
-        void DoRequestResponse(string text, Action<string> callback);
-    }
+		public int Registrations
+		{
+			get
+			{
+				if (ServerEvent != null)
+					return ServerEvent.GetInvocationList().Length;
+				else
+					return 0;
+			}
+		}
+	}
+	
+	public interface IRequestResponseCallbackSingleCall
+	{
+		void DoRequestResponse(string text, Action<string> callback);
+	}
 
-    public class RequestResponseCallbackSingleCall : IRequestResponseCallbackSingleCall
-    {
-        public void DoRequestResponse(string text, Action<string> callback)
-        {
-            callback(text + "!");
-        }
-    }
+	public class RequestResponseCallbackSingleCall : IRequestResponseCallbackSingleCall
+	{
+		public void DoRequestResponse(string text, Action<string> callback)
+		{
+			callback(text + "!");
+		}
+	}
 
-    public interface ITimerTriggeredEvent
-    {
-        event Action<DateTime> Tick;
+	public interface ITimerTriggeredEvent
+	{
+		event Action<DateTime> Tick;
 
-        void StartTimer();
-        void StopTimer();
-    }
+		void StartTimer();
+		void StopTimer();
+	}
 
-    public class TimerTriggeredEvent : ITimerTriggeredEvent
-    {
-        private Timer _timer = null;
+	public class TimerTriggeredEvent : ITimerTriggeredEvent
+	{
+		private Timer _timer = null;
 
-        public TimerTriggeredEvent()
-        {
-            _timer = new Timer(300);
-            _timer.Elapsed += new ElapsedEventHandler(_timer_Elapsed);
-        }
+		public TimerTriggeredEvent()
+		{
+			_timer = new Timer(300);
+			_timer.Elapsed += new ElapsedEventHandler(_timer_Elapsed);
+		}
 
+		private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			if (Tick != null)
+				Tick(DateTime.Now);
+		}
 
+		public event Action<DateTime> Tick;
+		
+		public void StartTimer()
+		{
+			_timer.Start();
+		}
 
-        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            if (Tick != null)
-                Tick(DateTime.Now);
-        }
+		public void StopTimer()
+		{
+			_timer.Stop();
+		}
+	}
 
-        public event Action<DateTime> Tick;
-        
-        public void StartTimer()
-        {
-            _timer.Start();
-        }
+	public class EventServerLocator : MarshalByRefObject
+	{ 
+		public EventServer GetEventServer()
+		{
+			return EventServer.Instance;
+		}
+	}
+	
+	public class EventServer : MarshalByRefObject, IDisposable
+	{
+		private static EventServer _instance = null;
 
-        public void StopTimer()
-        {
-            _timer.Stop();
-        }
-    }
+		public static EventServer Instance
+		{
+			get 
+			{
+				if (_instance == null)
+					_instance = new EventServer();
 
-    public class EventServerLocator : MarshalByRefObject
-    { 
-        public EventServer GetEventServer()
-        {
-            return EventServer.Instance;
-        }
-    }
-    
-    public class EventServer : MarshalByRefObject, IDisposable
-    {
-        private static EventServer _instance = null;
+				return _instance;
+			}
+		}
 
-        public static EventServer Instance
-        {
-            get 
-            {
-                if (_instance == null)
-                    _instance = new EventServer();
+		private ZyanComponentHost _host;
+		private ZyanComponentHost _duplexHost;
+		private ComponentCatalog _catalog;
+		
+		private EventServer ()
+		{
+			_catalog = new ComponentCatalog();
+			_catalog.RegisterComponent<IEventComponentSingleton, EventComponentSingleton>(ActivationType.Singleton);
+			_catalog.RegisterComponent<IEventComponentSingleCall, EventComponentSingleCall>(ActivationType.SingleCall);
+			_catalog.RegisterComponent<ICallbackComponentSingleton, CallbackComponentSingleton>(ActivationType.Singleton);
+			_catalog.RegisterComponent<ICallbackComponentSingleCall, CallbackComponentSingleCall>(ActivationType.SingleCall);
+			_catalog.RegisterComponent<IRequestResponseCallbackSingleCall, RequestResponseCallbackSingleCall>(ActivationType.SingleCall);
+			_catalog.RegisterComponent<ITimerTriggeredEvent, TimerTriggeredEvent>(ActivationType.Singleton);
+			
+			TcpCustomServerProtocolSetup protocol = new TcpCustomServerProtocolSetup(8083, new NullAuthenticationProvider(), true);
+			_host = new ZyanComponentHost("EventTest", protocol, _catalog);            
 
-                return _instance;
-            }
-        }
+			TcpDuplexServerProtocolSetup protocol2 = new TcpDuplexServerProtocolSetup(8084, new NullAuthenticationProvider(), true);
+			_duplexHost = new ZyanComponentHost("DuplexEventTest", protocol2, _catalog);            
+		}
 
-        private ZyanComponentHost _host;
-        private ZyanComponentHost _duplexHost;
-        private ComponentCatalog _catalog;
-        
-        private EventServer ()
-	    {
-            _catalog = new ComponentCatalog();
-            _catalog.RegisterComponent<IEventComponentSingleton, EventComponentSingleton>(ActivationType.Singleton);
-            _catalog.RegisterComponent<IEventComponentSingleCall, EventComponentSingleCall>(ActivationType.SingleCall);
-            _catalog.RegisterComponent<ICallbackComponentSingleton, CallbackComponentSingleton>(ActivationType.Singleton);
-            _catalog.RegisterComponent<ICallbackComponentSingleCall, CallbackComponentSingleCall>(ActivationType.SingleCall);
-            _catalog.RegisterComponent<IRequestResponseCallbackSingleCall, RequestResponseCallbackSingleCall>(ActivationType.SingleCall);
-            _catalog.RegisterComponent<ITimerTriggeredEvent, TimerTriggeredEvent>(ActivationType.Singleton);
-            
-            TcpCustomServerProtocolSetup protocol = new TcpCustomServerProtocolSetup(8083, new NullAuthenticationProvider(), true);
-            _host = new ZyanComponentHost("EventTest", protocol, _catalog);            
-
-            TcpDuplexServerProtocolSetup protocol2 = new TcpDuplexServerProtocolSetup(8084, new NullAuthenticationProvider(), true);
-            _duplexHost = new ZyanComponentHost("DuplexEventTest", protocol2, _catalog);            
-	    }
-
-        public void Dispose()
-        {
-            if (_host != null)
-            {
-                _host.Dispose();
-                _host = null;
-            }
-            if (_duplexHost != null)
-            {
-                _duplexHost.Dispose();
-                _duplexHost = null;
-            }
-        }
-    }
+		public void Dispose()
+		{
+			if (_host != null)
+			{
+				_host.Dispose();
+				_host = null;
+			}
+			if (_duplexHost != null)
+			{
+				_duplexHost.Dispose();
+				_duplexHost = null;
+			}
+		}
+	}
 }
