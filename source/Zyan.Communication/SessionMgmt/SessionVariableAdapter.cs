ec7babd4f4ded8f62e35fd96bc662a82cf56db7c
Alexey Yakovlev <yallie@yandex.ru>
2011-07-30 14:21:43 +0000

    [en] Added ISessionVariableAdapter interface. Marked SessionVariableAdapter class as internal. Added strong-typed GetSessionVariable<T> methods and indexer.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/cyan;C12536

--- source/Zyan.Communication/SessionMgmt/SessionVariableAdapter.cs
+++ source/Zyan.Communication/SessionMgmt/SessionVariableAdapter.cs
@@ -1,45 +1,89 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Zyan.Communication.SessionMgmt
 {
-    /// <summary>
-    /// Adapter für den Zugriff auf Sitzungsvariablen.
-    /// </summary>
-    public class SessionVariableAdapter
-    {
-        // Felder
-        private ISessionManager _sessionManager = null;
-        private Guid _sessionID = Guid.Empty;
+	/// <summary>
+	/// Session variable container.
+	/// </summary>
+	internal class SessionVariableAdapter : ISessionVariableAdapter
+	{
+		// Fields
+		private ISessionManager _sessionManager = null;
+		private Guid _sessionID = Guid.Empty;
 
-        /// <summary>
-        /// Erzeugt eine neue Instanz von SessionVariableAdapter.
-        /// </summary>
-        /// <param name="sessionManager">Sitzungsverwaltung</param>
-        /// <param name="sessionID">Sitzungsschlüssel</param>
-        internal SessionVariableAdapter(ISessionManager sessionManager, Guid sessionID)
-        {
-            _sessionManager = sessionManager;
-            _sessionID = sessionID;
-        }
+		/// <summary>
+		/// Initializes a new instance of SessionVariableAdapter.
+		/// </summary>
+		/// <param name="sessionManager">Session manager component.</param>
+		/// <param name="sessionID">Session unique identifier.</param>
+		internal SessionVariableAdapter(ISessionManager sessionManager, Guid sessionID)
+		{
+			_sessionManager = sessionManager;
+			_sessionID = sessionID;
+		}
 
-        /// <summary>
-        /// Legt den Wert einer Sitzungsvariablen fest.
-        /// </summary>        
-        /// <param name="name">Variablenname</param>
-        /// <param name="value">Wert</param>
-        public void SetSessionVariable(string name, object value)
-        {
-            _sessionManager.SetSessionVariable(_sessionID, name, value);
-        }
+		/// <summary>
+		/// Set the new value for a session variable with the given name.
+		/// </summary>
+		/// <param name="name">Variable name.</param>
+		/// <param name="value">Variable value.</param>
+		public void SetSessionVariable(string name, object value)
+		{
+			_sessionManager.SetSessionVariable(_sessionID, name, value);
+		}
 
-        /// <summary>
-        /// Gibt den Wert einer Sitzungsvariablen zurück.
-        /// </summary>        
-        /// <param name="name">Variablenname</param>
-        /// <returns>Wert</returns>
-        public object GetSessionVariable(string name)
-        {
-            return _sessionManager.GetSessionVariable(_sessionID, name);
-        }
-    }
+		/// <summary>
+		/// Get an untyped value of the session variable with the given name.
+		/// </summary>
+		/// <param name="name">Variable name</param>
+		/// <returns>
+		/// Variable value.
+		/// </returns>
+		public object GetSessionVariable(string name)
+		{
+			return _sessionManager.GetSessionVariable(_sessionID, name);
+		}
+
+		/// <summary>
+		/// Get strongly-typed value of the session variable with the given name.
+		/// </summary>
+		/// <typeparam name="T">Variable type.</typeparam>
+		/// <param name="name">Variable name.</param>
+		/// <returns>
+		/// Variable value.
+		/// </returns>
+		public T GetSessionVariable<T>(string name)
+		{
+			var isValueType = typeof(ValueType).IsAssignableFrom(typeof(T));
+			var defaultValue = isValueType ? Activator.CreateInstance<T>() : (T)(object)null;
+			return GetSessionVariable(name, defaultValue);
+		}
+
+		/// <summary>
+		/// Get strongly-typed value of the session variable with the given name.
+		/// </summary>
+		/// <typeparam name="T">Variable type.</typeparam>
+		/// <param name="name">Variable name.</param>
+		/// <param name="defaultValue">Default value to return if the variable is not defined.</param>
+		/// <returns>
+		/// Variable value.
+		/// </returns>
+		public T GetSessionVariable<T>(string name, T defaultValue)
+		{
+			var value = GetSessionVariable(name) ?? (object)defaultValue;
+			return (T)(object)value;
+		}
+
+		/// <summary>
+		/// Gets or sets the variable value.
+		/// </summary>
+		/// <param name="variableName">Variable name.</param>
+		/// <returns>The value of the session variable.</returns>
+		public object this[string variableName]
+		{
+			get { return GetSessionVariable(variableName); }
+			set { SetSessionVariable(variableName, value); }
+		}
+	}
 }
