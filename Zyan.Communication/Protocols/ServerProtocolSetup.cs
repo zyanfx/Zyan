5460a4f65930d856cf3ae0563069bad631755532
Alexey Yakovlev <yallie@users.noreply.github.com>
2011-08-16 09:15:56 +0000

    [en] ScriptEngine now throws an exception instead of returning null assembly.

--- Zyan.Communication/Protocols/ServerProtocolSetup.cs
+++ Zyan.Communication/Protocols/ServerProtocolSetup.cs
@@ -9,173 +9,173 @@ using System.Runtime.Remoting;
 
 namespace Zyan.Communication.Protocols
 {
-    /// <summary>
-    /// General implementation of server protocol setup.
-    /// </summary>
-    public class ServerProtocolSetup : IServerProtocolSetup
-    {   
-        /// <summary>
-        /// Unique channel name.
-        /// </summary>
-        protected string _channelName = "ServerProtocolSetup_" + Guid.NewGuid().ToString();
-        
-        /// <summary>
-        /// Dictionary for channel settings.
-        /// </summary>
-        protected Dictionary<string, object> _channelSettings = new Dictionary<string, object>();
-        
-        /// <summary>
-        /// List for building the client sink chain.
-        /// </summary>
-        protected List<IClientChannelSinkProvider> _clientSinkChain = new List<IClientChannelSinkProvider>();
+	/// <summary>
+	/// General implementation of server protocol setup.
+	/// </summary>
+	public class ServerProtocolSetup : IServerProtocolSetup
+	{
+		/// <summary>
+		/// Unique channel name.
+		/// </summary>
+		protected string _channelName = "ServerProtocolSetup_" + Guid.NewGuid().ToString();
 
-        /// <summary>
-        /// List for building the server sink chain.
-        /// </summary>
-        protected List<IServerChannelSinkProvider> _serverSinkChain = new List<IServerChannelSinkProvider>();
+		/// <summary>
+		/// Dictionary for channel settings.
+		/// </summary>
+		protected Dictionary<string, object> _channelSettings = new Dictionary<string, object>();
 
-        /// <summary>
-        /// Delegate to factory method, which creates the .NET Remoting channel instance.
-        /// </summary>
-        protected Func<IDictionary, IClientChannelSinkProvider, IServerChannelSinkProvider, IChannel> _channelFactory = null;
-        
-        /// <summary>
-        /// Authentication provider.
-        /// </summary>
-        protected IAuthenticationProvider _authProvider = new NullAuthenticationProvider();
+		/// <summary>
+		/// List for building the client sink chain.
+		/// </summary>
+		protected List<IClientChannelSinkProvider> _clientSinkChain = new List<IClientChannelSinkProvider>();
 
-        /// <summary>
-        /// Creates a new instance of the ServerProtocolSetupBase class.
-        /// </summary>
-        public ServerProtocolSetup() { }
+		/// <summary>
+		/// List for building the server sink chain.
+		/// </summary>
+		protected List<IServerChannelSinkProvider> _serverSinkChain = new List<IServerChannelSinkProvider>();
 
-        /// <summary>
-        /// Creates a new instance of the ServerProtocolSetup class.
-        /// </summary>
-        /// <param name="channelFactory">Delegate to channel factory method</param>
-        public ServerProtocolSetup(Func<IDictionary, IClientChannelSinkProvider, IServerChannelSinkProvider, IChannel> channelFactory) : this()
-        {
-            if (channelFactory == null)
-                throw new ArgumentNullException("channelFactory");
-                        
-            _channelFactory = channelFactory;
-        }
+		/// <summary>
+		/// Delegate to factory method, which creates the .NET Remoting channel instance.
+		/// </summary>
+		protected Func<IDictionary, IClientChannelSinkProvider, IServerChannelSinkProvider, IChannel> _channelFactory = null;
 
-        /// <summary>
-        /// Creates a new ServerProtocolSetup with a specified channel factory method.
-        /// </summary>
-        /// <param name="channelFactory">Delegate to channel factory method</param>
-        /// <returns></returns>
-        public static IServerProtocolSetup WithChannel(Func<IDictionary, IClientChannelSinkProvider, IServerChannelSinkProvider, IChannel> channelFactory)
-        {
-            return new ServerProtocolSetup(channelFactory);
-        }
+		/// <summary>
+		/// Authentication provider.
+		/// </summary>
+		protected IAuthenticationProvider _authProvider = new NullAuthenticationProvider();
 
-        /// <summary>
-        /// Gets a dictionary with channel settings.
-        /// </summary>
-        public virtual Dictionary<string, object> ChannelSettings { get { return _channelSettings; } }
+		/// <summary>
+		/// Creates a new instance of the ServerProtocolSetupBase class.
+		/// </summary>
+		public ServerProtocolSetup() { }
 
-        /// <summary>
-        /// Gets a list of all Remoting sinks from the client sink chain.
-        /// </summary>
-        public virtual List<IClientChannelSinkProvider> ClientSinkChain { get { return _clientSinkChain; } }
+		/// <summary>
+		/// Creates a new instance of the ServerProtocolSetup class.
+		/// </summary>
+		/// <param name="channelFactory">Delegate to channel factory method</param>
+		public ServerProtocolSetup(Func<IDictionary, IClientChannelSinkProvider, IServerChannelSinkProvider, IChannel> channelFactory) : this()
+		{
+			if (channelFactory == null)
+				throw new ArgumentNullException("channelFactory");
 
-        /// <summary>
-        /// Gets a list of all Remoting sinks from the server sink chain.
-        /// </summary>
-        public virtual List<IServerChannelSinkProvider> ServerSinkChain { get { return _serverSinkChain; } }
+			_channelFactory = channelFactory;
+		}
 
-        /// <summary>
-        /// Builds the client sink chain.
-        /// </summary>
-        /// <returns>First sink provider in sink chain</returns>
-        protected virtual IClientChannelSinkProvider BuildClientSinkChain()
-        {
-            IClientChannelSinkProvider firstProvider = null;
-            IClientChannelSinkProvider lastProvider = null;
+		/// <summary>
+		/// Creates a new ServerProtocolSetup with a specified channel factory method.
+		/// </summary>
+		/// <param name="channelFactory">Delegate to channel factory method</param>
+		/// <returns></returns>
+		public static IServerProtocolSetup WithChannel(Func<IDictionary, IClientChannelSinkProvider, IServerChannelSinkProvider, IChannel> channelFactory)
+		{
+			return new ServerProtocolSetup(channelFactory);
+		}
 
-            foreach (var clientProvider in _clientSinkChain)
-            {
-                if (firstProvider == null)
-                    firstProvider = clientProvider;
+		/// <summary>
+		/// Gets a dictionary with channel settings.
+		/// </summary>
+		public virtual Dictionary<string, object> ChannelSettings { get { return _channelSettings; } }
 
-                if (lastProvider == null)
-                    lastProvider = clientProvider;
-                else
-                {
-                    lastProvider.Next = clientProvider;
-                    lastProvider = clientProvider;
-                }
-            }
-            return firstProvider;
-        }
+		/// <summary>
+		/// Gets a list of all Remoting sinks from the client sink chain.
+		/// </summary>
+		public virtual List<IClientChannelSinkProvider> ClientSinkChain { get { return _clientSinkChain; } }
 
-        /// <summary>
-        /// Builds the server sink chain.
-        /// </summary>
-        /// <returns>First sink provider in sink chain</returns>
-        protected virtual IServerChannelSinkProvider BuildServerSinkChain()
-        {
-            IServerChannelSinkProvider firstProvider = null;
-            IServerChannelSinkProvider lastProvider = null;
+		/// <summary>
+		/// Gets a list of all Remoting sinks from the server sink chain.
+		/// </summary>
+		public virtual List<IServerChannelSinkProvider> ServerSinkChain { get { return _serverSinkChain; } }
 
-            foreach (var clientProvider in _serverSinkChain)
-            {
-                if (firstProvider == null)
-                    firstProvider = clientProvider;
+		/// <summary>
+		/// Builds the client sink chain.
+		/// </summary>
+		/// <returns>First sink provider in sink chain</returns>
+		protected virtual IClientChannelSinkProvider BuildClientSinkChain()
+		{
+			IClientChannelSinkProvider firstProvider = null;
+			IClientChannelSinkProvider lastProvider = null;
 
-                if (lastProvider == null)
-                    lastProvider = clientProvider;
-                else
-                {
-                    lastProvider.Next = clientProvider;
-                    lastProvider = clientProvider;
-                }
-            }
-            return firstProvider;
-        }
-                
-        /// <summary>
-        /// Creates and configures a Remoting channel.        
-        /// </summary>
-        /// <returns>Remoting channel</returns>
-        public virtual IChannel CreateChannel()
-        {
-            IChannel channel = ChannelServices.GetChannel(_channelName);
+			foreach (var clientProvider in _clientSinkChain)
+			{
+				if (firstProvider == null)
+					firstProvider = clientProvider;
 
-            if (channel == null)
-            {
-                if (_channelFactory == null)
-                    throw new ApplicationException(LanguageResource.ApplicationException_NoChannelFactorySpecified);
+				if (lastProvider == null)
+					lastProvider = clientProvider;
+				else
+				{
+					lastProvider.Next = clientProvider;
+					lastProvider = clientProvider;
+				}
+			}
+			return firstProvider;
+		}
 
-                _channelSettings["name"] = _channelName;
+		/// <summary>
+		/// Builds the server sink chain.
+		/// </summary>
+		/// <returns>First sink provider in sink chain</returns>
+		protected virtual IServerChannelSinkProvider BuildServerSinkChain()
+		{
+			IServerChannelSinkProvider firstProvider = null;
+			IServerChannelSinkProvider lastProvider = null;
 
-                channel = _channelFactory(_channelSettings, BuildClientSinkChain(), BuildServerSinkChain());
+			foreach (var clientProvider in _serverSinkChain)
+			{
+				if (firstProvider == null)
+					firstProvider = clientProvider;
 
-                if (!MonoCheck.IsRunningOnMono)
-                {
-                    if (RemotingConfiguration.CustomErrorsMode != CustomErrorsModes.Off)
-                        RemotingConfiguration.CustomErrorsMode = CustomErrorsModes.Off;
-                }
-                return channel;
-            }
-            return null;
-        }
+				if (lastProvider == null)
+					lastProvider = clientProvider;
+				else
+				{
+					lastProvider.Next = clientProvider;
+					lastProvider = clientProvider;
+				}
+			}
+			return firstProvider;
+		}
 
-        /// <summary>
-        /// Gets or sets the authentication provider.
-        /// </summary>
-        public virtual IAuthenticationProvider AuthenticationProvider
-        {
-            get { return _authProvider; }
-            set
-            {
-                if (value == null)
-                    _authProvider = new NullAuthenticationProvider();
-                else
-                    _authProvider = value;
-            }
-        }
-    }
+		/// <summary>
+		/// Creates and configures a Remoting channel.
+		/// </summary>
+		/// <returns>Remoting channel</returns>
+		public virtual IChannel CreateChannel()
+		{
+			IChannel channel = ChannelServices.GetChannel(_channelName);
+
+			if (channel == null)
+			{
+				if (_channelFactory == null)
+					throw new ApplicationException(LanguageResource.ApplicationException_NoChannelFactorySpecified);
+
+				_channelSettings["name"] = _channelName;
+
+				channel = _channelFactory(_channelSettings, BuildClientSinkChain(), BuildServerSinkChain());
+
+				if (!MonoCheck.IsRunningOnMono)
+				{
+					if (RemotingConfiguration.CustomErrorsMode != CustomErrorsModes.Off)
+						RemotingConfiguration.CustomErrorsMode = CustomErrorsModes.Off;
+				}
+				return channel;
+			}
+			return null;
+		}
+
+		/// <summary>
+		/// Gets or sets the authentication provider.
+		/// </summary>
+		public virtual IAuthenticationProvider AuthenticationProvider
+		{
+			get { return _authProvider; }
+			set
+			{
+				if (value == null)
+					_authProvider = new NullAuthenticationProvider();
+				else
+					_authProvider = value;
+			}
+		}
+	}
 }
