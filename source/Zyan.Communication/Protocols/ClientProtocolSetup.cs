fc7ca27788b1cdda0ed7d60e90b796f795a35afe
yallie_cp <SND\yallie_cp>
2011-08-16 09:15:56 +0000

    [en] ScriptEngine now throws an exception instead of returning null assembly.

--- source/Zyan.Communication/Protocols/ClientProtocolSetup.cs
+++ source/Zyan.Communication/Protocols/ClientProtocolSetup.cs
@@ -8,147 +8,147 @@ using System.Runtime.Remoting;
 
 namespace Zyan.Communication.Protocols
 {
-    /// <summary>
-    /// General implementation of client protocol setup.
-    /// </summary>
-    public class ClientProtocolSetup : IClientProtocolSetup
-    {
-        /// <summary>
-        /// Unique channel name.
-        /// </summary>
-        protected string _channelName = "ClientProtocolSetup_" + Guid.NewGuid().ToString();
+	/// <summary>
+	/// General implementation of client protocol setup.
+	/// </summary>
+	public class ClientProtocolSetup : IClientProtocolSetup
+	{
+		/// <summary>
+		/// Unique channel name.
+		/// </summary>
+		protected string _channelName = "ClientProtocolSetup_" + Guid.NewGuid().ToString();
 
-        /// <summary>
-        /// Dictionary for channel settings.
-        /// </summary>
-        protected Dictionary<string, object> _channelSettings = new Dictionary<string, object>();
+		/// <summary>
+		/// Dictionary for channel settings.
+		/// </summary>
+		protected Dictionary<string, object> _channelSettings = new Dictionary<string, object>();
 
-        /// <summary>
-        /// List for building the client sink chain.
-        /// </summary>
-        protected List<IClientChannelSinkProvider> _clientSinkChain=new List<IClientChannelSinkProvider>();
+		/// <summary>
+		/// List for building the client sink chain.
+		/// </summary>
+		protected List<IClientChannelSinkProvider> _clientSinkChain = new List<IClientChannelSinkProvider>();
 
-        /// <summary>
-        /// List for building the server sink chain.
-        /// </summary>
-        protected List<IServerChannelSinkProvider> _serverSinkChain = new List<IServerChannelSinkProvider>();
+		/// <summary>
+		/// List for building the server sink chain.
+		/// </summary>
+		protected List<IServerChannelSinkProvider> _serverSinkChain = new List<IServerChannelSinkProvider>();
 
-        /// <summary>
-        /// Delegate to factory method, which creates the .NET Remoting channel instance.
-        /// </summary>
-        protected Func<IDictionary, IClientChannelSinkProvider, IServerChannelSinkProvider, IChannel> _channelFactory = null;
+		/// <summary>
+		/// Delegate to factory method, which creates the .NET Remoting channel instance.
+		/// </summary>
+		protected Func<IDictionary, IClientChannelSinkProvider, IServerChannelSinkProvider, IChannel> _channelFactory = null;
 
-        /// <summary>
-        /// Creates a new instance of the ClientProtocolSetup class.
-        /// </summary>
-        public ClientProtocolSetup() { }
-        
-        /// <summary>
-        /// Creates a new instance of the ClientProtocolSetup class.
-        /// </summary>
-        /// <param name="channelFactory">Delegate to channel factory method</param>
-        public ClientProtocolSetup(Func<IDictionary, IClientChannelSinkProvider, IServerChannelSinkProvider, IChannel> channelFactory) 
-        {
-            if (channelFactory == null)
-                throw new ArgumentNullException("channelFactory");
-                        
-            _channelFactory = channelFactory;
-        }
-        
-        /// <summary>
-        /// Creates a new ClientProtocolSetup with a specified channel factory method.
-        /// </summary>
-        /// <param name="channelFactory">Delegate to channel factory method</param>
-        /// <returns></returns>
-        public static IClientProtocolSetup WithChannel(Func<IDictionary, IClientChannelSinkProvider, IServerChannelSinkProvider, IChannel> channelFactory)
-        {
-            return new ClientProtocolSetup(channelFactory);            
-        }
+		/// <summary>
+		/// Creates a new instance of the ClientProtocolSetup class.
+		/// </summary>
+		public ClientProtocolSetup() { }
 
-        /// <summary>
-        /// Gets a dictionary with channel settings.
-        /// </summary>
-        public virtual Dictionary<string, object> ChannelSettings { get { return _channelSettings; } }
+		/// <summary>
+		/// Creates a new instance of the ClientProtocolSetup class.
+		/// </summary>
+		/// <param name="channelFactory">Delegate to channel factory method</param>
+		public ClientProtocolSetup(Func<IDictionary, IClientChannelSinkProvider, IServerChannelSinkProvider, IChannel> channelFactory)
+		{
+			if (channelFactory == null)
+				throw new ArgumentNullException("channelFactory");
 
-        /// <summary>
-        /// Gets a list of all Remoting sinks from the client sink chain.
-        /// </summary>
-        public virtual List<IClientChannelSinkProvider> ClientSinkChain { get { return _clientSinkChain; } }
+			_channelFactory = channelFactory;
+		}
 
-        /// <summary>
-        /// Gets a list of all Remoting sinks from the server sink chain.
-        /// </summary>
-        public virtual List<IServerChannelSinkProvider> ServerSinkChain { get { return _serverSinkChain; } }
-                
-        /// <summary>
-        /// Builds the client sink chain.
-        /// </summary>
-        /// <returns>First sink provider in sink chain</returns>
-        protected virtual IClientChannelSinkProvider BuildClientSinkChain()
-        {
-            IClientChannelSinkProvider firstProvider = null;
-            IClientChannelSinkProvider lastProvider = null;
+		/// <summary>
+		/// Creates a new ClientProtocolSetup with a specified channel factory method.
+		/// </summary>
+		/// <param name="channelFactory">Delegate to channel factory method</param>
+		/// <returns></returns>
+		public static IClientProtocolSetup WithChannel(Func<IDictionary, IClientChannelSinkProvider, IServerChannelSinkProvider, IChannel> channelFactory)
+		{
+			return new ClientProtocolSetup(channelFactory);
+		}
 
-            foreach (var clientProvider in _clientSinkChain)
-            {
-                if (firstProvider == null)
-                    firstProvider = clientProvider;
+		/// <summary>
+		/// Gets a dictionary with channel settings.
+		/// </summary>
+		public virtual Dictionary<string, object> ChannelSettings { get { return _channelSettings; } }
 
-                if (lastProvider == null)
-                    lastProvider = clientProvider;
-                else
-                    lastProvider.Next = clientProvider;
-            }
-            return firstProvider;
-        }
+		/// <summary>
+		/// Gets a list of all Remoting sinks from the client sink chain.
+		/// </summary>
+		public virtual List<IClientChannelSinkProvider> ClientSinkChain { get { return _clientSinkChain; } }
 
-        /// <summary>
-        /// Builds the server sink chain.
-        /// </summary>
-        /// <returns>First sink provider in sink chain</returns>
-        protected virtual IServerChannelSinkProvider BuildServerSinkChain()
-        {
-            IServerChannelSinkProvider firstProvider = null;
-            IServerChannelSinkProvider lastProvider = null;
+		/// <summary>
+		/// Gets a list of all Remoting sinks from the server sink chain.
+		/// </summary>
+		public virtual List<IServerChannelSinkProvider> ServerSinkChain { get { return _serverSinkChain; } }
 
-            foreach (var clientProvider in _serverSinkChain)
-            {
-                if (firstProvider == null)
-                    firstProvider = clientProvider;
+		/// <summary>
+		/// Builds the client sink chain.
+		/// </summary>
+		/// <returns>First sink provider in sink chain</returns>
+		protected virtual IClientChannelSinkProvider BuildClientSinkChain()
+		{
+			IClientChannelSinkProvider firstProvider = null;
+			IClientChannelSinkProvider lastProvider = null;
 
-                if (lastProvider == null)
-                    lastProvider = clientProvider;
-                else
-                    lastProvider.Next = clientProvider;
-            }
-            return firstProvider;
-        }
+			foreach (var clientProvider in _clientSinkChain)
+			{
+				if (firstProvider == null)
+					firstProvider = clientProvider;
 
-        /// <summary>
-        /// Creates and configures a Remoting channel.        
-        /// </summary>
-        /// <returns>Remoting channel</returns>
-        public virtual IChannel CreateChannel()
-        {
-            IChannel channel = ChannelServices.GetChannel(_channelName);
+				if (lastProvider == null)
+					lastProvider = clientProvider;
+				else
+					lastProvider.Next = clientProvider;
+			}
+			return firstProvider;
+		}
 
-            if (channel == null)
-            {
-                if (_channelFactory == null)
-                    throw new ApplicationException(LanguageResource.ApplicationException_NoChannelFactorySpecified);
+		/// <summary>
+		/// Builds the server sink chain.
+		/// </summary>
+		/// <returns>First sink provider in sink chain</returns>
+		protected virtual IServerChannelSinkProvider BuildServerSinkChain()
+		{
+			IServerChannelSinkProvider firstProvider = null;
+			IServerChannelSinkProvider lastProvider = null;
 
-                _channelSettings["name"] = _channelName;
+			foreach (var clientProvider in _serverSinkChain)
+			{
+				if (firstProvider == null)
+					firstProvider = clientProvider;
 
-                channel = _channelFactory(_channelSettings, BuildClientSinkChain(), BuildServerSinkChain());
-                
-                if (!MonoCheck.IsRunningOnMono)
-                {
-                    if (RemotingConfiguration.CustomErrorsMode != CustomErrorsModes.Off)
-                        RemotingConfiguration.CustomErrorsMode = CustomErrorsModes.Off;
-                }
-                return channel;
-            }
-            return null;
-        }
-    }
+				if (lastProvider == null)
+					lastProvider = clientProvider;
+				else
+					lastProvider.Next = clientProvider;
+			}
+			return firstProvider;
+		}
+
+		/// <summary>
+		/// Creates and configures a Remoting channel.
+		/// </summary>
+		/// <returns>Remoting channel</returns>
+		public virtual IChannel CreateChannel()
+		{
+			IChannel channel = ChannelServices.GetChannel(_channelName);
+
+			if (channel == null)
+			{
+				if (_channelFactory == null)
+					throw new ApplicationException(LanguageResource.ApplicationException_NoChannelFactorySpecified);
+
+				_channelSettings["name"] = _channelName;
+
+				channel = _channelFactory(_channelSettings, BuildClientSinkChain(), BuildServerSinkChain());
+
+				if (!MonoCheck.IsRunningOnMono)
+				{
+					if (RemotingConfiguration.CustomErrorsMode != CustomErrorsModes.Off)
+						RemotingConfiguration.CustomErrorsMode = CustomErrorsModes.Off;
+				}
+				return channel;
+			}
+			return null;
+		}
+	}
 }
--- source/Zyan.Communication/Scripting/ScriptEngine.cs
+++ source/Zyan.Communication/Scripting/ScriptEngine.cs
@@ -1,68 +1,80 @@
 ﻿using Microsoft.CSharp;
 using System;
+using System.Text;
 using System.CodeDom;
 using System.CodeDom.Compiler;
 using System.Reflection;
 
 namespace Zyan.Communication.Scripting
 {
-    /// <summary>
-    /// Compiles C# source code on runtime.
-    /// </summary>
-    public static class ScriptEngine
-    {
-        /// <summary>
-        /// Creates a compiled assembly from source code.
-        /// </summary>
-        /// <param name="scriptCode">C# source code</param>
-        /// <param name="referenceAsseblies">Array with assembly file paths to reference</param>
-        /// <returns>Compiled assembly</returns>
-        public static Assembly CompileScriptToAssembly(string scriptCode, params string[] referenceAsseblies)
-        {
-            CompilerParameters cp = new CompilerParameters()
-            {
-                GenerateExecutable=false,
-                GenerateInMemory=true,                
-            };            
-            if (referenceAsseblies.Length > 0)
-                cp.ReferencedAssemblies.AddRange(referenceAsseblies);
-            else
-                cp.ReferencedAssemblies.AddRange(new string[] 
-                {
-                    "System.dll",
-                    "System.Core.dll",
-                    "System.Xml.dll",
-                    "System.Data.dll",
-                    "System.Data.DataSetExtensions.dll"
-                });
-            
-            using (CSharpCodeProvider codeProvider = new CSharpCodeProvider())
-            {
-                CompilerResults results = codeProvider.CompileAssemblyFromSource(cp, scriptCode);
+	/// <summary>
+	/// Compiles C# source code on runtime.
+	/// </summary>
+	public static class ScriptEngine
+	{
+		/// <summary>
+		/// Creates a compiled assembly from source code.
+		/// </summary>
+		/// <param name="scriptCode">C# source code</param>
+		/// <param name="referenceAsseblies">Array with assembly file paths to reference</param>
+		/// <returns>Compiled assembly</returns>
+		public static Assembly CompileScriptToAssembly(string scriptCode, params string[] referenceAsseblies)
+		{
+			CompilerParameters cp = new CompilerParameters()
+			{
+				GenerateExecutable = false,
+				GenerateInMemory = true,
+			};
 
-                if (results.Errors.Count == 0)
-                    return results.CompiledAssembly;
-            }
-            return null;
-        }
+			if (referenceAsseblies.Length > 0)
+				cp.ReferencedAssemblies.AddRange(referenceAsseblies);
+			else
+				cp.ReferencedAssemblies.AddRange(new[]
+				{
+					"System.dll",
+					"System.Core.dll",
+					"System.Xml.dll",
+					"System.Data.dll",
+					"System.Data.DataSetExtensions.dll"
+				});
 
-        /// <summary>
-        /// Gets the C# name of a specified type.
-        /// </summary>
-        /// <param name="type">Type</param>
-        /// <returns>C# name</returns>
-        public static string GetCSharpNameOfType(Type type)
-        {
-            if (type == null)
-                throw new ArgumentNullException("type");
+			using (CSharpCodeProvider codeProvider = new CSharpCodeProvider())
+			{
+				CompilerResults results = codeProvider.CompileAssemblyFromSource(cp, scriptCode);
 
-            if (!type.IsGenericType)
-                return type.FullName.Replace('+','.');
+				if (results.Errors.Count == 0)
+				{
+					return results.CompiledAssembly;
+				}
 
-            CodeTypeReference typeRef = new CodeTypeReference(type);
-            CSharpCodeProvider provider = new CSharpCodeProvider();
+				var errorReport = new StringBuilder();
+				errorReport.AppendLine("Error creating dynamic wire.");
+				foreach (CompilerError error in results.Errors)
+				{
+					errorReport.AppendLine(error.ErrorText);
+				}
 
-            return provider.GetTypeOutput(typeRef).Replace('+','.');
-        }
-    }
+				throw new Exception(errorReport.ToString());
+			}
+		}
+
+		/// <summary>
+		/// Gets the C# name of a specified type.
+		/// </summary>
+		/// <param name="type">Type</param>
+		/// <returns>C# name</returns>
+		public static string GetCSharpNameOfType(Type type)
+		{
+			if (type == null)
+				throw new ArgumentNullException("type");
+
+			if (!type.IsGenericType)
+				return type.FullName.Replace('+', '.');
+
+			CodeTypeReference typeRef = new CodeTypeReference(type);
+			CSharpCodeProvider provider = new CSharpCodeProvider();
+
+			return provider.GetTypeOutput(typeRef).Replace('+', '.');
+		}
+	}
 }
