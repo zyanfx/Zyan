0f419a4c20d715f50827fa257c8750e35dae7eac
yallie_cp <SND\yallie_cp>
2011-08-17 14:50:28 +0000

    [en] Rewritten DynamicWireFactory for new dynamic wire classes, removed ScriptEngine. Dynamic wires are now created using very fast cached compiled Linq expressions. Wiring code for events and delegates in ZyanDispatcher doesn't use reflection anymore.

---
+++ source/Zyan.Communication/DynamicWireT.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Zyan.Communication
+{
+	/// <summary>
+	/// Strongly typed wrapper for DelegateInterceptor.
+	/// </summary>
+	/// <typeparam name="T">Delegate type.</typeparam>
+	public class DynamicWire<T> : DynamicWireBase
+	{
+		/// <summary>
+		/// Initializes <see cref="DynamicWire{T}"/> instance.
+		/// </summary>
+		public DynamicWire()
+		{
+			In = BuildDelegate<T>();
+		}
+
+		/// <summary>
+		/// Dynamic wire delegate.
+		/// </summary>
+		public T In { get; private set; }
+
+		/// <summary>
+		/// Gets the untyped In delegate.
+		/// </summary>
+		public override Delegate InDelegate
+		{
+			get { return (Delegate)(object)In; }
+		}
+	}
+}
