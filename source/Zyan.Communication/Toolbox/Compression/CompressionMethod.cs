d7453685ea322e54ac50dfa54996068ec833c41f
Alexey Yakovlev <yallie@yandex.ru>
2011-10-23 14:11:21 +0000

    [en] Added LZF, CompressionHelper, CompressionMethod enumeration. Added unit tests for CompressionHelper class.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/cyan;C13986

---
+++ source/Zyan.Communication/Toolbox/Compression/CompressionMethod.cs
@@ -0,0 +1,28 @@
+﻿namespace Zyan.Communication.Toolbox.Compression
+{
+	/// <summary>
+	/// Compression levels.
+	/// </summary>
+	public enum CompressionMethod
+	{
+		/// <summary>
+		/// No compression.
+		/// </summary>
+		None,
+
+		/// <summary>
+		/// Fast compression, poor ratio (LZF).
+		/// </summary>
+		Fast,
+
+		/// <summary>
+		/// Average compression, better ratio (DeflateStream).
+		/// </summary>
+		Average,
+
+		/// <summary>
+		/// Default compression level is Fast.
+		/// </summary>
+		Default = Fast
+	}
+}
