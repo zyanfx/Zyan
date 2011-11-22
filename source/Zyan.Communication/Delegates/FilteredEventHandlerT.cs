b8aa6563c9735432f3889da417a1c28830f107a8
yallie_cp <SND\yallie_cp>
2011-11-22 16:50:11 +0000

    [en] Renamed FilteredEventHandler<T> to FilteredCustomHandler<T>, replaced compiled lambda-expression with dynamic method to preserve Target reference. Added filter chain support to FilteredEventHandler<T> and FilteredCustomHandler<T>. Fixed a bug in CombinedEventFilter class.

--- source/Zyan.Communication/Delegates/FilteredEventHandlerT.cs
+++ source/Zyan.Communication/Delegates/FilteredEventHandlerT.cs
@@ -2,73 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Reflection;
 
 namespace Zyan.Communication.Delegates
 {
 	/// <summary>
-	/// Represents filtered event handler of a non-standard type.
+	/// Represents filtered event handler of type EventHandler{TEventArgs}.
 	/// </summary>
-	/// <typeparam name="TDelegate">The type of the event handler delegate.</typeparam>
-	internal class FilteredEventHandler<TDelegate> : IFilteredEventHandler
-		where TDelegate : class
+	/// <typeparam name="TEventArgs">The type of the event args.</typeparam>
+	internal class FilteredEventHandler<TEventArgs> : IFilteredEventHandler where TEventArgs : EventArgs
 	{
 		/// <summary>
-		/// Initializes a new instance of the <see cref="FilteredEventHandler&lt;TDelegate&gt;"/> class.
+		/// Initializes a new instance of the <see cref="FilteredEventHandler&lt;TEventArgs&gt;"/> class.
 		/// </summary>
 		/// <param name="eventHandler">The event handler.</param>
 		/// <param name="eventFilter">The event filter.</param>
-		public FilteredEventHandler(TDelegate eventHandler, IEventFilter eventFilter)
+		public FilteredEventHandler(EventHandler<TEventArgs> eventHandler, IEventFilter eventFilter)
 		{
-			if (!(eventHandler is Delegate))
-			{
-				throw new ArgumentOutOfRangeException("eventHandler");
-			}
+			IEventFilter sourceFilter;
+			ExtractSourceHandler(eventHandler, out eventHandler, out sourceFilter);
 
 			EventHandler = eventHandler;
-			EventFilter = eventFilter;
-
-			// create strong-typed invoke method
-			TypedInvoke = DynamicWireFactory.BuildDelegate<TDelegate>(InvokeMethodInfo, this);
+			EventFilter = eventFilter.Combine(sourceFilter);
 		}
 
-		/// <summary>
-		/// Untyped Invoke() method.
-		/// </summary>
-		private object Invoke(params object[] args)
+		private void ExtractSourceHandler(EventHandler<TEventArgs> eventHandler, out EventHandler<TEventArgs> sourceHandler, out IEventFilter sourceFilter)
+		{
+			sourceHandler = eventHandler;
+			sourceFilter = default(IEventFilter);
+
+			while (sourceHandler.Target is IFilteredEventHandler)
+			{
+				var filtered = sourceHandler.Target as IFilteredEventHandler;
+				sourceHandler = filtered.EventHandler as EventHandler<TEventArgs>;
+				sourceFilter = filtered.EventFilter.Combine(sourceFilter);
+			}
+		}
+
+		private void Invoke(object sender, TEventArgs args)
 		{
 			// filter event at the client-side, if invoked locally
-			if (EventFilter != null && !EventFilter.AllowInvocation(args))
+			if (EventFilter != null && !EventFilter.AllowInvocation(sender, args))
 			{
-				return null;
+				return;
 			}
 
 			// invoke client handler
-			var eventHandler = (this as IFilteredEventHandler).EventHandler;
-			if (eventHandler != null)
+			if (EventHandler != null)
 			{
-				return eventHandler.DynamicInvoke(args);
+				EventHandler(sender, args);
 			}
-
-			return null;
 		}
-
-		/// <summary>
-		/// Strong-typed Invoke() method, built dynamically.
-		/// Calls the untyped Invoke() method to do the real job.
-		/// </summary>
-		private TDelegate TypedInvoke { get; set; }
-
-		/// <summary>
-		/// MethodInfo for the private Invoke(args) method.
-		/// </summary>
-		private static MethodInfo InvokeMethodInfo = typeof(FilteredEventHandler<TDelegate>).GetMethod("Invoke",
-			BindingFlags.Instance | BindingFlags.NonPublic, null, new[] { typeof(object[]) }, null);
 
 		/// <summary>
 		/// Gets the event handler.
 		/// </summary>
-		public TDelegate EventHandler { get; private set; }
+		public EventHandler<TEventArgs> EventHandler { get; private set; }
 
 		/// <summary>
 		/// Gets the event filter.
@@ -76,21 +64,21 @@ namespace Zyan.Communication.Delegates
 		public IEventFilter EventFilter { get; private set; }
 
 		/// <summary>
-		/// Performs an implicit conversion from <see cref="Zyan.Communication.Delegates.FilteredEventHandler&lt;TEventFilter&gt;"/>
-		/// to <see cref="System.EventHandler"/>.
+		/// Performs an implicit conversion from <see cref="Zyan.Communication.Delegates.FilteredEventHandler&lt;TEventArgs&gt;"/>
+		/// to <see cref="System.EventHandler&lt;TEventArgs&gt;"/>.
 		/// </summary>
 		/// <param name="filteredEventHandler">The filtered event handler.</param>
 		/// <returns>
 		/// The result of the conversion.
 		/// </returns>
-		public static implicit operator TDelegate(FilteredEventHandler<TDelegate> filteredEventHandler)
+		public static implicit operator EventHandler<TEventArgs>(FilteredEventHandler<TEventArgs> filteredEventHandler)
 		{
-			return filteredEventHandler.TypedInvoke;
+			return filteredEventHandler.Invoke;
 		}
 
 		Delegate IFilteredEventHandler.EventHandler
 		{
-			get { return (Delegate)(object)EventHandler; }
+			get { return EventHandler; }
 		}
 
 		IEventFilter IFilteredEventHandler.EventFilter
