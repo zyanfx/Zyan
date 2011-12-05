39c8eb14910c84c3187e31345dbec056eadd4fd2
Hagen Siegel <hagen.siegel@the-rainbird.de>
2011-12-05 20:24:17 +0000

    [en] - Added NewLogonNeededEventArgs.cs to Mono-Project - Added sample code for Reconnect feature to MiniChat example [de] - NewLogonNeededEventArgs.cs zu Mono-Projekt zurgefügt - Beispielcode für Reconnect-feature zum MiniChat-Beispiel zugefügt
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/cyan;C15187

--- examples/Zyan.Examples.MiniChat/Zyan.Examples.MiniChat.Client/Program.cs
+++ examples/Zyan.Examples.MiniChat/Zyan.Examples.MiniChat.Client/Program.cs
@@ -5,12 +5,17 @@ using Zyan.Communication;
 using Zyan.Examples.MiniChat.Shared;
 using System.Collections;
 using System.Security;
+using System.Threading;
+using System.Net.Sockets;
+using System.Runtime.Remoting;
 
 namespace Zyan.Examples.MiniChat.Client
 {
     static class Program
     {
         private static ZyanConnection _connection;
+
+        private static Hashtable _credentials;
 
         public static ZyanConnection ServerConnection
         { get { return _connection; } }
@@ -42,15 +47,21 @@ namespace Zyan.Examples.MiniChat.Client
             if (string.IsNullOrEmpty(nickname))
                 return;
 
-            Hashtable credentials = new Hashtable();
-            credentials.Add("nickname", nickname);
+            _credentials = new Hashtable();
+            _credentials.Add("nickname", nickname);
 
             TcpDuplexClientProtocolSetup protocol = new TcpDuplexClientProtocolSetup(true);
             
             try
             {
-                using (_connection = new ZyanConnection(serverUrl, protocol, credentials, false, true))
+                using (_connection = new ZyanConnection(serverUrl, protocol, _credentials, false, true))
                 {
+                    _connection.PollingInterval = new TimeSpan(0, 0, 30);
+                    _connection.PollingEnabled = true;
+                    _connection.Disconnected += new EventHandler<DisconnectedEventArgs>(_connection_Disconnected);
+                    _connection.NewLogonNeeded += new EventHandler<NewLogonNeededEventArgs>(_connection_NewLogonNeeded);                    
+                    _connection.Error += new EventHandler<ZyanErrorEventArgs>(_connection_Error);
+
                     _connection.CallInterceptors.For<IMiniChat>()
                         .Add<string, string>(
                             (chat, nickname2, text) => chat.SendMessage(nickname2, text),
@@ -72,6 +83,38 @@ namespace Zyan.Examples.MiniChat.Client
             {
                 MessageBox.Show(ex.Message);
             }
+        }
+
+        static void _connection_Error(object sender, ZyanErrorEventArgs e)
+        {
+            if (e.Exception is SocketException || e.Exception is InvalidSessionException || e.Exception is RemotingException)
+            {
+                int retryCount = 0;
+                bool problemSolved = false;
+
+                while (!problemSolved && retryCount < 10)
+                {
+                    Thread.Sleep(5000);
+                    problemSolved = _connection.Reconnect();
+                    retryCount++;
+                }
+                if (problemSolved)
+                    e.Action = ZyanErrorAction.Retry;
+                else
+                    e.Action = ZyanErrorAction.ThrowException;
+            }
+            else
+                e.Action = ZyanErrorAction.ThrowException;
+        }
+
+        static void _connection_NewLogonNeeded(object sender, NewLogonNeededEventArgs e)
+        {
+            e.Credentials = _credentials;
+        }
+
+        static void _connection_Disconnected(object sender, DisconnectedEventArgs e)
+        {           
+            e.Retry = e.RetryCount < 6;            
         }
     }
 }
