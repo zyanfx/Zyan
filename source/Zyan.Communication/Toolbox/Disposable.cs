999d7d949a27955e2efc1f8bf73ea64b271a74f9
Alexey Yakovlev <yallie@yandex.ru>
2011-12-21 20:24:21 +0000

    [en] Implemented thread-static call interception pause mode. It allows calling any remote methods from interceptor code.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/cyan;C15705

---
+++ source/Zyan.Communication/Toolbox/Disposable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zyan.Communication.Toolbox
+{
+	/// <summary>
+	/// Helper class for operations that require deterministic release of resources.
+	/// </summary>
+	internal class Disposable : IDisposable
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Disposable"/> class.
+		/// </summary>
+		/// <param name="cleanupAction">The cleanup action.</param>
+		public Disposable(Action cleanupAction)
+		{
+			CleanupAction = cleanupAction;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Disposable"/> class.
+		/// </summary>
+		public Disposable()
+			: this(null)
+		{
+		}
+
+		private Action CleanupAction { get; set; }
+
+		/// <summary>
+		/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+		/// </summary>
+		public void Dispose()
+		{
+			if (CleanupAction != null)
+			{
+				CleanupAction();
+				CleanupAction = null;
+			}
+		}
+	}
+}
