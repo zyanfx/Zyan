10a63d197e29a9010056f5b1162898638f02547e
Hagen Siegel <hagen.siegel@the-rainbird.de>
2012-03-01 05:34:29 +0000

    [en] Skip resolving registration, if already done in ZyanDispatcher.Invoke_ResolveComponentInstance.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/cyan;C17746

--- source/Zyan.Communication/ZyanDispatcher.cs
+++ source/Zyan.Communication/ZyanDispatcher.cs
@@ -343,8 +343,11 @@ namespace Zyan.Communication
 		/// <param name="details">Invocation details</param>
 		private void Invoke_ResolveComponentInstance(InvocationDetails details)
 		{
-			// get component instance
-			details.Registration = _host.ComponentRegistry[details.InterfaceName];
+            // Skip resolving registration, if already done
+            if (details.Registration == null)
+                details.Registration = _host.ComponentRegistry[details.InterfaceName];
+
+			// get component instance			
 			details.Instance = _host.GetComponentInstance(details.Registration);
 			details.Type = details.Instance.GetType();
 		}
