612ef69c731ccca27555c0b35d3620e3548befc7
Alexey Yakovlev <yallie@users.noreply.github.com>
2012-04-01 19:48:38 +0000

    [en] Added ProtocolSetup classes for the NullChannel.

---
+++ Zyan.Communication/Protocols/Null/NullServerProtocolSetup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Remoting.Channels;
+using System.Threading;
+using IDictionary = System.Collections.IDictionary;
+
+namespace Zyan.Communication.Protocols.Null
+{
+	/// <summary>
+	/// <see cref="IServerProtocolSetup"/> implementation for the <see cref="NullChannel"/>.
+	/// </summary>
+	public class NullServerProtocolSetup : ServerProtocolSetup
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="NullServerProtocolSetup"/> class.
+		/// </summary>
+		public NullServerProtocolSetup(int port)
+			: base((props, clientSinkProvider, serverSinkProvider) => new NullChannel(props, clientSinkProvider, serverSinkProvider))
+		{
+			_channelName = "NullChannel:" + port;
+		}
+	}
+}
