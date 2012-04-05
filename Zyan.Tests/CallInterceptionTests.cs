9f4834f2b278822e6695137ab4c98ad3162a252f
Alexey Yakovlev <yallie@users.noreply.github.com>
2012-04-05 13:19:15 +0000

    [en] Added unit tests for call interceptors. Tests for subscription and unsubscription fail.

---
+++ Zyan.Tests/CallInterceptionTests.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using Zyan.Communication;
+using Zyan.Communication.Protocols.Null;
+using Zyan.Communication.SessionMgmt;
+
+namespace Zyan.Tests
+{
+	#region Unit testing platform abstraction layer
+#if NUNIT
+	using NUnit.Framework;
+	using TestClass = NUnit.Framework.TestFixtureAttribute;
+	using TestMethod = NUnit.Framework.TestAttribute;
+	using ClassInitializeNonStatic = NUnit.Framework.TestFixtureSetUpAttribute;
+	using ClassInitialize = DummyAttribute;
+	using ClassCleanupNonStatic = NUnit.Framework.TestFixtureTearDownAttribute;
+	using ClassCleanup = DummyAttribute;
+	using TestContext = System.Object;
+	using TestInitialize = NUnit.Framework.SetUpAttribute;
+	using TestCleanup = NUnit.Framework.TearDownAttribute;
+#else
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using ClassInitializeNonStatic = DummyAttribute;
+	using ClassCleanupNonStatic = DummyAttribute;
+#endif
+	#endregion
+
+	/// <summary>
+	/// Test class for strong-typed call interceptor builder.
+	/// </summary>
+	[TestClass]
+	public class CallInterceptionTests
+	{
+		#region Interfaces and components
+
+		public interface IInterceptableComponent
+		{
+			void Procedure();
+
+			void Procedure(long arg1, TimeSpan arg2, short arg3, IInterceptableComponent arg4);
+
+			string Function(int a, DateTime b, TimeSpan c);
+
+			event EventHandler ProcedureCalled;
+		}
+
+		public class InterceptableComponent : IInterceptableComponent
+		{
+			public void Procedure()
+			{
+				OnProcedureCalled();
+			}
+
+			public void Procedure(long arg1, TimeSpan arg2, short arg3, IInterceptableComponent arg4)
+			{
+				OnProcedureCalled();
+			}
+
+			public string Function(int a, DateTime b, TimeSpan c)
+			{
+				return string.Format("Called with arguments: {0}, {1}, {2}", a, b, c);
+			}
+
+			public event EventHandler ProcedureCalled;
+
+			private void OnProcedureCalled()
+			{
+				if (ProcedureCalled != null)
+					ProcedureCalled(null, EventArgs.Empty);
+			}
+		}
+
+		#endregion
+
+		#region Initialization and cleanup
+
+		public TestContext TestContext { get; set; }
+
+		private static ZyanComponentHost ZyanHost { get; set; }
+
+		private static ZyanConnection ZyanConnection { get; set; }
+
+		[ClassInitializeNonStatic]
+		public void Initialize()
+		{
+			StartServer(null);
+		}
+
+		[ClassCleanupNonStatic]
+		public void Cleanup()
+		{
+		}
+
+		[ClassInitialize]
+		public static void StartServer(TestContext ctx)
+		{
+			var serverSetup = new NullServerProtocolSetup(3456);
+			ZyanHost = new ZyanComponentHost("CallInterceptorServer", serverSetup);
+			ZyanHost.RegisterComponent<IInterceptableComponent, InterceptableComponent>(ActivationType.Singleton);
+
+			var clientSetup = new NullClientProtocolSetup();
+			ZyanConnection = new ZyanConnection("null://NullChannel:3456/CallInterceptorServer", clientSetup);
+			ZyanConnection.CallInterceptionEnabled = true;
+		}
+
+		[ClassCleanup]
+		public static void StopServer()
+		{
+			ZyanConnection.Dispose();
+			ZyanHost.Dispose();
+		}
+
+		[TestCleanup]
+		public void ClearCallInterceptors()
+		{
+			ZyanConnection.CallInterceptors.Clear();
+		}
+
+		#endregion
+
+		[TestMethod]
+		public void ParameterlessVoidMethod_IsIntercepted()
+		{
+			var flag = false;
+
+			ZyanConnection.CallInterceptors.For<IInterceptableComponent>().Add(
+				component => component.Procedure(),
+				data =>
+				{
+					flag = true;
+					data.Intercepted = true;
+				});
+
+			var proxy = ZyanConnection.CreateProxy<IInterceptableComponent>();
+			proxy.Procedure();
+			Assert.IsTrue(flag);
+		}
+
+		[TestMethod]
+		public void VoidMethodWithParameters_IsIntercepted()
+		{
+			var flag = false;
+
+			var interceptor = CallInterceptor.For<IInterceptableComponent>().Action<long, TimeSpan, short, IInterceptableComponent>(
+				(component, arg1, arg2, arg3, arg4) => component.Procedure(arg1, arg2, arg3, arg4),
+				(data, arg1, arg2, arg3, arg4) =>
+				{
+					if (arg1 == 123)
+					{
+						flag = true;
+						data.Intercepted = true;
+					}
+				});
+
+			ZyanConnection.CallInterceptors.Add(interceptor);
+			var proxy = ZyanConnection.CreateProxy<IInterceptableComponent>();
+
+			// not intercepted
+			proxy.Procedure(321, TimeSpan.FromSeconds(1), 0, null);
+			Assert.IsFalse(flag);
+
+			// intercepted
+			proxy.Procedure(123, TimeSpan.FromSeconds(1), 0, null);
+			Assert.IsTrue(flag);
+		}
+
+		[TestMethod]
+		public void FunctionWithParameters_IsIntercepted()
+		{
+			const string interceptedResult = "Intercepted!";
+
+			var interceptor = CallInterceptor.For<IInterceptableComponent>().Func<int, DateTime, TimeSpan, string>(
+				(component, arg1, arg2, arg3) => component.Function(arg1, arg2, arg3),
+				(data, arg1, arg2, arg3) =>
+				{
+					if (arg1 == 123)
+					{
+						data.Intercepted = true;
+						return interceptedResult;
+					}
+
+					// not intercepted
+					return null;
+				});
+
+			ZyanConnection.CallInterceptors.Add(interceptor);
+			var proxy = ZyanConnection.CreateProxy<IInterceptableComponent>();
+
+			// not intercepted
+			var result = proxy.Function(321, DateTime.Now, TimeSpan.FromSeconds(1));
+			Assert.AreNotEqual(interceptedResult, result);
+
+			// intercepted
+			result = proxy.Function(123, DateTime.Today, TimeSpan.FromSeconds(1));
+			Assert.AreEqual(interceptedResult, result);
+		}
+
+		[TestMethod]
+		public void EventHandlerSubscriptionIsIntercepted()
+		{
+			var intercepted = false;
+			var procedureCalled = false;
+
+			var interceptor = new CallInterceptor(typeof(IInterceptableComponent),
+				MemberTypes.Method, "add_ProcedureCalled", new[] { typeof(EventHandler) }, data =>
+				{
+					intercepted = true;
+				});
+
+			ZyanConnection.CallInterceptors.Add(interceptor);
+
+			var proxy = ZyanConnection.CreateProxy<IInterceptableComponent>();
+			proxy.ProcedureCalled += (sender, args) => procedureCalled = true;
+			Assert.IsFalse(procedureCalled);
+			Assert.IsTrue(intercepted);
+
+			proxy.Procedure();
+			Assert.IsTrue(procedureCalled);
+		}
+
+		[TestMethod]
+		public void EventHandlerUnsubscriptionIsIntercepted()
+		{
+			var intercepted = false;
+			var procedureCalled = false;
+
+			var interceptor = new CallInterceptor(typeof(IInterceptableComponent),
+				MemberTypes.Method, "remove_ProcedureCalled", new[] { typeof(EventHandler) }, data =>
+				{
+					intercepted = true;
+				});
+
+			ZyanConnection.CallInterceptors.Add(interceptor);
+
+			var proxy = ZyanConnection.CreateProxy<IInterceptableComponent>();
+			var handler = new EventHandler((sender, args) => procedureCalled = true);
+			proxy.ProcedureCalled += handler;
+			Assert.IsFalse(procedureCalled);
+			Assert.IsFalse(intercepted);
+
+			proxy.Procedure();
+			Assert.IsTrue(procedureCalled);
+			Assert.IsFalse(intercepted);
+
+			proxy.ProcedureCalled -= handler;
+			Assert.IsTrue(procedureCalled);
+			Assert.IsTrue(intercepted);
+		}
+	}
+}
