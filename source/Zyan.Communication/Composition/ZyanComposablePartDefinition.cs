3f69e7b7796d24e3e27f4082b7796a41cbb5b91c
Alexey Yakovlev <yallie@yandex.ru>
2012-09-04 11:49:20 +0000

    [en] Added ZyanComposablePartDefinition.ToString() overload. Added XML comments to the ZyanComposablePart and ZyanComposablePartDefinition classes.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/cyan;C24628

--- source/Zyan.Communication/Composition/ZyanComposablePartDefinition.cs
+++ source/Zyan.Communication/Composition/ZyanComposablePartDefinition.cs
@@ -14,6 +14,13 @@ namespace Zyan.Communication.Composition
 	/// </summary>
 	internal class ZyanComposablePartDefinition : ComposablePartDefinition
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ZyanComposablePartDefinition" /> class.
+		/// </summary>
+		/// <param name="connection">The connection.</param>
+		/// <param name="componentInterfaceName">Name of the component interface.</param>
+		/// <param name="uniqueName">Unique name of the published component.</param>
+		/// <param name="transferTransactions">if set to <c>true</c>, then ambient transaction transfer is enabled.</param>
 		public ZyanComposablePartDefinition(ZyanConnection connection, string componentInterfaceName, string uniqueName, bool transferTransactions)
 		{
 			Connection = connection;
@@ -25,14 +32,29 @@ namespace Zyan.Communication.Composition
 
 		private object lockObject = new object();
 
+		/// <summary>
+		/// Gets the <see cref="ZyanConnection"/> used to create transparent proxies.
+		/// </summary>
 		public ZyanConnection Connection { get; private set; }
 
+		/// <summary>
+		/// Gets the unique name of the component (typically, the same as ComponentInterfaceName).
+		/// </summary>
 		public string ComponentUniqueName { get; private set; }
 
+		/// <summary>
+		/// Gets the name of the component interface.
+		/// </summary>
 		public string ComponentInterfaceName { get; private set; }
 
+		/// <summary>
+		/// Gets the component interface.
+		/// </summary>
 		public Lazy<Type> ComponentInterface { get; private set; }
 
+		/// <summary>
+		/// Gets a value indicating whether implicit transaction transfer is enabled.
+		/// </summary>
 		public bool ImplicitTransactionTransfer { get; private set; }
 
 		/// <summary>
@@ -52,6 +74,12 @@ namespace Zyan.Communication.Composition
 			return assemblyQualifiedName.Substring(0, commaIndex);
 		}
 
+		/// <summary>
+		/// Creates a new instance of a part that the <see cref="T:System.ComponentModel.Composition.Primitives.ComposablePartDefinition" /> describes.
+		/// </summary>
+		/// <returns>
+		/// The created part.
+		/// </returns>
 		public override ComposablePart CreatePart()
 		{
 			return new ZyanComposablePart(this);
@@ -59,6 +87,13 @@ namespace Zyan.Communication.Composition
 
 		private IEnumerable<ExportDefinition> exportDefinitions = null;
 
+		/// <summary>
+		/// Gets a collection of <see cref="T:System.ComponentModel.Composition.Primitives.ExportDefinition" /> objects that describe the objects
+		/// exported by the part defined by this <see cref="T:System.ComponentModel.Composition.Primitives.ComposablePartDefinition" /> object.
+		/// </summary>
+		/// <returns>A collection of <see cref="T:System.ComponentModel.Composition.Primitives.ExportDefinition" /> objects
+		/// that describe the exported objects provided by <see cref="T:System.ComponentModel.Composition.Primitives.ComposablePart" /> objects
+		/// created by the <see cref="T:System.ComponentModel.Composition.Primitives.ComposablePartDefinition" />.</returns>
 		public override IEnumerable<ExportDefinition> ExportDefinitions
 		{
 			get
@@ -86,9 +121,34 @@ namespace Zyan.Communication.Composition
 			}
 		}
 
+		/// <summary>
+		/// Gets a collection of <see cref="T:System.ComponentModel.Composition.Primitives.ImportDefinition" /> objects that describe the imports
+		/// required by the part defined by this <see cref="T:System.ComponentModel.Composition.Primitives.ComposablePartDefinition" /> object.
+		/// </summary>
+		/// <returns>A collection of <see cref="T:System.ComponentModel.Composition.Primitives.ImportDefinition" /> objects 
+		/// that describe the imports required by <see cref="T:System.ComponentModel.Composition.Primitives.ComposablePart" /> objects 
+		/// created by the <see cref="T:System.ComponentModel.Composition.Primitives.ComposablePartDefinition" />.</returns>
 		public override IEnumerable<ImportDefinition> ImportDefinitions
 		{
 			get { return new ImportDefinition[0]; }
+		}
+
+		/// <summary>
+		/// Returns a <see cref="System.String" /> that represents this instance.
+		/// </summary>
+		/// <returns>
+		/// A <see cref="System.String" /> that represents this instance.
+		/// </returns>
+		public override string ToString()
+		{
+			var result = ComponentInterfaceName;
+
+			if (ComponentUniqueName != ComponentInterfaceName)
+			{
+				result += " (" + ComponentUniqueName + ")";
+			}
+
+			return result + " // zyan";
 		}
 	}
 }
