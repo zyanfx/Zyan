e0d9b6af146e26bf4f8b58119ab8e3b184e637f7
Alexey Yakovlev <yallie@yandex.ru>
2013-06-19 15:25:37 +0000

    [en] Enabled encryption in Android sample application. Added an input box for the server address. Client application caches ZyanConnection instance and executes remote calls asynchronously so the UI doesn't freeze.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/cyan;C33944

--- examples/Zyan.Examples.Android/Zyan.Examples.Android.Client/Resources/Resource.designer.cs
+++ examples/Zyan.Examples.Android/Zyan.Examples.Android.Client/Resources/Resource.designer.cs
@@ -60,8 +60,14 @@ namespace Zyan.Examples.Android.Client
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050001
+			public const int myButton = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int responseView = 2131034114;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int serverEditText = 2131034112;
 			
 			static Id()
 			{
