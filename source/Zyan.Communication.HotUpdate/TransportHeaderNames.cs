ba76b5ea4b995576509557cf4de74f3a57f1a67a
Hagen Siegel <hagen.siegel@the-rainbird.de>
2014-03-03 23:28:06 +0000

    Implemented client side envelope sink for hot-update support
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/cyan;C39825

---
+++ source/Zyan.Communication.HotUpdate/TransportHeaderNames.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zyan.Communication.HotUpdate
+{
+    internal static class TransportHeaderNames
+    {
+        internal const string HEADER_ENVELOPE = "X-CY_ENVELOPE";
+    }
+}
