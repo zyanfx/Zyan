828c6008ce4b8c01a7ae2b82289c75b329ff99e7
Alexey Yakovlev <yallie@yandex.ru>
2014-05-26 12:01:19 +0000

    [en] Added discovery metadata classes and unit tests. Added stub classes for discovery client and server (feature #2085).
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/cyan;C41122

---
+++ source/Zyan.Communication/Discovery/Metadata/DiscoveryRequest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zyan.Communication.Discovery.Metadata
+{
+	/// <summary>
+	/// Discovery request metadata packet.
+	/// </summary>
+	public class DiscoveryRequest : DiscoveryMetadata
+	{
+		/// <summary>
+		/// Packet signature.
+		/// </summary>
+		public const string SignatureString = "ZyanDiscoveryRequest";
+
+		internal DiscoveryRequest()
+			: base(SignatureString)
+		{ 
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DiscoveryRequest"/> class.
+		/// </summary>
+		/// <param name="namePattern"><see cref="ZyanComponentHost"/> name pattern (regular expression).</param>
+		/// <param name="version">The version.</param>
+		public DiscoveryRequest(string namePattern, string version = null)
+			: base(SignatureString)
+		{
+			NamePattern = namePattern;
+			Version = null;
+		}
+
+		/// <summary>
+		/// Gets or sets the <see cref="ZyanComponentHost"/> name pattern.
+		/// </summary>
+		public string NamePattern
+		{
+			get { return Properties["NamePattern"]; }
+			set { Properties["NamePattern"] = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets the required server version.
+		/// </summary>
+		public string Version
+		{
+			get { return Properties["Version"]; }
+			set { Properties["Version"] = value; }
+		}
+	}
+}
