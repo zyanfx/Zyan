7836c353792ec4ff303e79ed97019d29c5921c0b
Alexey Yakovlev <yallie@yandex.ru>
2014-05-26 20:12:56 +0000

    [en] Implemented DiscoveryClient and DiscoveryServer using UdpClient. Extracted static methods from DiscoveryMetadata class into DiscoveryMetadataHelper (#2085).
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/cyan;C41130

--- source/Zyan.Communication/Discovery/DiscoveryClient.cs
+++ source/Zyan.Communication/Discovery/DiscoveryClient.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+using Zyan.Communication.Discovery.Metadata;
+using Zyan.Communication.Toolbox.Diagnostics;
 
 namespace Zyan.Communication.Discovery
 {
@@ -11,11 +16,91 @@ namespace Zyan.Communication.Discovery
 	public class DiscoveryClient : IDisposable
 	{
 		/// <summary>
+		/// Initializes a new instance of the <see cref="DiscoveryClient"/> class.
+		/// </summary>
+		/// <param name="requestMetadata">The request metadata.</param>
+		/// <param name="port">The port of the discovery server.</param>
+		public DiscoveryClient(DiscoveryMetadata requestMetadata, int port = DiscoveryServer.DefaultDiscoveryPort)
+		{
+			if (requestMetadata == null)
+			{
+				throw new ArgumentNullException("requestMetadata", "Discovery request metadata is missing.");
+			}
+
+			Port = port;
+			RequestMetadata = requestMetadata;
+			RequestMetadataPacket = DiscoveryMetadataHelper.Encode(RequestMetadata);
+			DestinationAddress = IPAddress.Broadcast;
+			DestinationEndpoint = new IPEndPoint(DestinationAddress, Port);
+			Results = new HashSet<DiscoveryMetadata>();
+			RetryTimeout = TimeSpan.FromSeconds(1);
+			RetryCount = 10;
+		}
+
+		/// <summary>
+		/// Gets or sets the request metadata.
+		/// </summary>
+		public DiscoveryMetadata RequestMetadata { get; set; }
+
+		private byte[] RequestMetadataPacket { get; set; }
+
+		/// <summary>
+		/// Gets or sets the port of the discovery server.
+		/// </summary>
+		public int Port { get; set; }
+
+		/// <summary>
+		/// Gets a value indicating whether discovery is stopped.
+		/// </summary>
+		public bool Stopped { get; private set; }
+
+		private IPAddress DestinationAddress { get; set; }
+
+		private IPEndPoint DestinationEndpoint { get; set; }
+
+		private UdpClient UdpClient { get; set; }
+
+		private TimeSpan RetryTimeout { get; set; }
+
+		private int RetryCount { get; set; }
+
+		private int Counter { get; set; }
+
+		private Timer RetryTimer { get; set; }
+
+		private HashSet<DiscoveryMetadata> Results { get; set; }
+
+		private object lockObject = new object();
+
+		/// <summary>
+		/// Occurs when <see cref="DiscoveryServer"/> response is acquired.
+		/// </summary>
+		public event EventHandler<DiscoveryEventArgs> Discovered;
+
+		/// <summary>
+		/// Raises the <see cref="E:Discovered" /> event.
+		/// </summary>
+		/// <param name="e">The <see cref="DiscoveryEventArgs"/> instance containing the event data.</param>
+		protected virtual void OnDiscovered(DiscoveryEventArgs e)
+		{
+			var handler = Discovered;
+			if (handler != null)
+			{
+				handler(this, e);
+			}
+		}
+
+		/// <summary>
 		/// Starts the discovery.
 		/// </summary>
 		public void StartDiscovery()
-		{
-			throw new NotImplementedException();
+		{ 
+			Stopped = false;
+			Counter = RetryCount;
+			Results.Clear();
+
+			UdpClient = new UdpClient();
+			RetryTimer = new Timer(DiscoveryTimerCallback, null, TimeSpan.Zero, RetryTimeout);
 		}
 
 		/// <summary>
@@ -23,7 +108,19 @@ namespace Zyan.Communication.Discovery
 		/// </summary>
 		public void StopDiscovery()
 		{
-			throw new NotImplementedException();
+			Stopped = true;
+
+			if (UdpClient != null)
+			{
+				UdpClient.Close();
+				UdpClient = null;
+			}
+
+			if (RetryTimer != null)
+			{
+				RetryTimer.Dispose();
+				RetryTimer = null;
+			}
 		}
 
 		/// <summary>
@@ -34,18 +131,92 @@ namespace Zyan.Communication.Discovery
 			StopDiscovery();
 		}
 
-		/// <summary>
-		/// Occurs when <see cref="DiscoveryServer"/> response is acquired.
-		/// </summary>
-		public event EventHandler Discovered;
-
-		private void OnDiscovered()
+		private void DiscoveryTimerCallback(object state)
 		{
-			var discovered = Discovered;
-			if (discovered != null)
+			if (Counter <= 0)
 			{
-				discovered(this, EventArgs.Empty);
+				StopDiscovery();
+				return;
 			}
+
+			if (!Stopped && UdpClient != null && Counter > 0)
+			{
+				Counter--;
+				SafePerform(() =>
+				{
+					UdpClient.BeginSend(RequestMetadataPacket, RequestMetadataPacket.Length, DestinationEndpoint, SendCallback, UdpClient);
+				});
+			}
+		}
+
+		private void SafePerform(Action udpAction)
+		{
+			// Suggested here: http://stackoverflow.com/questions/840090/where-is-udpclient-cancelreceive
+			try
+			{
+				lock (lockObject)
+				{
+					udpAction();
+				}
+			}
+			catch (SocketException ex)
+			{
+				// connection is closed
+				Trace.WriteLine("Connection is closed. Exception: {0}", ex);
+			}
+			catch (ObjectDisposedException ex)
+			{
+				// UdpClient is closed
+				Trace.WriteLine("Oops! UDP client is closed. Exception: {0}", ex);
+			}
+		}
+
+		private void SendCallback(IAsyncResult asyncResult)
+		{
+			SafePerform(() =>
+			{
+				var udpClient = (UdpClient)asyncResult.AsyncState;
+				udpClient.EndSend(asyncResult);
+				if (!Stopped)
+				{
+					udpClient.BeginReceive(ReceiveCallback, udpClient);
+				}
+			});
+		}
+
+		private void ReceiveCallback(IAsyncResult asyncResult)
+		{
+			var response = default(DiscoveryMetadata);
+			SafePerform(() =>
+			{
+				var udpClient = (UdpClient)asyncResult.AsyncState;
+				var remoteEndpoint = default(IPEndPoint);
+				var responseMetadata = udpClient.EndReceive(asyncResult, ref remoteEndpoint);
+				if (!Stopped)
+				{
+					response = DiscoveryMetadataHelper.Decode(responseMetadata);
+				}
+			});
+
+			if (response != null)
+			{
+				HandleResponse(response);
+			}
+		}
+
+		private void HandleResponse(DiscoveryMetadata response)
+		{
+			lock (lockObject)
+			{
+				if (response == null || Results.Contains(response))
+				{
+					return;
+				}
+
+				Results.Add(response);
+			}
+
+			OnDiscovered(new DiscoveryEventArgs(response));
 		}
 	}
 }
---
+++ source/Zyan.Communication/Discovery/Metadata/DiscoveryMetadataHelper.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Zyan.Communication.Toolbox;
+
+namespace Zyan.Communication.Discovery.Metadata
+{
+	/// <summary>
+	/// Helper methods for discovery metadata packets.
+	/// </summary>
+	public static class DiscoveryMetadataHelper
+	{
+		/// <summary>
+		/// Initializes the <see cref="DiscoveryMetadataHelper"/> class.
+		/// </summary>
+		static DiscoveryMetadataHelper()
+		{
+			// register standard discovery types
+			DiscoveryMetadataFactories = new Dictionary<string, Func<DiscoveryMetadata>>();
+			RegisterDiscoveryMetadataFactory(DiscoveryRequest.SignatureString, () => new DiscoveryRequest());
+			RegisterDiscoveryMetadataFactory(DiscoveryResponse.SignatureString, () => new DiscoveryResponse());
+		}
+
+		private static Dictionary<string, Func<DiscoveryMetadata>> DiscoveryMetadataFactories { get; set; }
+
+		/// <summary>
+		/// Registers the discovery metadata factory to create metadata packets given their signatures.
+		/// </summary>
+		/// <param name="signature">The signature.</param>
+		/// <param name="factory">The factory.</param>
+		public static void RegisterDiscoveryMetadataFactory(string signature, Func<DiscoveryMetadata> factory)
+		{
+			DiscoveryMetadataFactories[signature] = factory;
+		}
+
+		/// <summary>
+		/// Creates the discovery metadata packed based on the given packet signature.
+		/// </summary>
+		/// <param name="signature">The signature.</param>
+		public static DiscoveryMetadata CreateDiscoveryMetadata(string signature)
+		{
+			Func<DiscoveryMetadata> factory = null;
+			if (DiscoveryMetadataFactories.TryGetValue(signature, out factory))
+			{
+				return factory();
+			}
+
+			return null;
+		}
+
+		/// <summary>
+		/// Escapes the specified string.
+		/// </summary>
+		/// <param name="s">The string to escape.</param>
+		public static string Escape(string s)
+		{
+			if (s == null)
+			{
+				return "@";
+			}
+
+			return s.Replace(@"\", @"\s").Replace(@"|", @"\p").Replace(@"=", @"\q").Replace("@", @"\a");
+		}
+
+		/// <summary>
+		/// Unescapes the specified string.
+		/// </summary>
+		/// <param name="s">Escaped string to unescape.</param>
+		public static string Unescape(string s)
+		{
+			if (s == null || s == "@")
+			{
+				return null;
+			}
+
+			return s.Replace(@"\a", "@").Replace(@"\q", @"=").Replace(@"\p", "|").Replace(@"\s", @"\");
+		}
+
+		/// <summary>
+		/// Encodes the specified discovery metadata packet into byte array.
+		/// </summary>
+		/// <param name="dp">The packet to encode.</param>
+		public static byte[] Encode(DiscoveryMetadata dp)
+		{
+			// convert the packet into string
+			var sb = new StringBuilder();
+			sb.AppendFormat("{0}|", Escape(dp.Signature));
+			foreach (var entry in dp.Properties)
+			{
+				sb.AppendFormat("{0}={1}|", Escape(entry.Key), Escape(entry.Value));
+			}
+
+			// calculate checksum and merge
+			var content = Encoding.UTF8.GetBytes(sb.ToString());
+			var crc = new Crc32Calculator();
+			crc.UpdateWithBlock(content, 0, content.Length);
+			var checksum = BitConverter.GetBytes(crc.Crc32);
+			var contentLength = BitConverter.GetBytes(content.Length);
+
+			// packet contents: length, checksum, content
+			return contentLength.Concat(checksum).Concat(content).ToArray();
+		}
+
+		/// <summary>
+		/// Decodes the specified discovery metadata packet.
+		/// </summary>
+		/// <param name="packet">The packet to decode.</param>
+		public static DiscoveryMetadata Decode(byte[] packet)
+		{
+			// packet contains a length and a checksum at very least
+			if (packet.Length < sizeof(Int32) * 2)
+			{
+				return null;
+			}
+
+			// check content length
+			var contentLength = BitConverter.ToInt32(packet, 0);
+			if (packet.Length != contentLength + sizeof(Int32) * 2) // content + length + checksum
+			{
+				return null;
+			}
+
+			// validate checksum
+			var checksum = BitConverter.ToUInt32(packet, sizeof(Int32));
+			var crc = new Crc32Calculator();
+			crc.UpdateWithBlock(packet, sizeof(Int32) * 2, contentLength);
+			if (checksum != crc.Crc32)
+			{
+				return null;
+			}
+
+			// decode contents
+			var contents = packet.Skip(sizeof(Int32) * 2).ToArray();
+			var data = Encoding.UTF8.GetString(contents).Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+			if (data.Length < 1)
+			{
+				return null;
+			}
+
+			// extract signature and create the packet
+			var signature = Unescape(data.First());
+			var dp = CreateDiscoveryMetadata(signature);
+			if (dp == null)
+			{
+				return null;
+			}
+
+			// populate properties
+			foreach (var pair in data.Skip(1))
+			{
+				var entry = pair.Split('=');
+				dp.Properties[Unescape(entry.First())] = Unescape(entry.Last());
+			}
+
+			return dp;
+		}
+	}
+}
