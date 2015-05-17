4a0d32c6b17f938ac0709e5e39ec9671d7f7fd09
Alexey Yakovlev <yallie@yandex.ru>
2015-05-17 20:36:37 +0000

    [en] Added InterfaceName, MethodName, Arguments and DelegateCorrelationSet to InvokeCanceledEventArgs, issue #2450.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/cyan;C44088

--- source/Zyan.Communication/InvokeCanceledEventArgs.cs
+++ source/Zyan.Communication/InvokeCanceledEventArgs.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Zyan.Communication.Delegates;
 
 namespace Zyan.Communication
 {
@@ -16,5 +18,49 @@ namespace Zyan.Communication
 		/// Gets or sets the exception in case of cancellation.
 		/// </summary>
 		public Exception CancelException { get; set; }
+
+		/// <summary>
+		/// Gets or sets the interface name of the remote component.
+		/// </summary>
+		public string InterfaceName { get; set; }
+
+		/// <summary>
+		/// Gets or sets the correlation set for wiring remote delegates.
+		/// </summary>
+		public List<DelegateCorrelationInfo> DelegateCorrelationSet { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name of the remote method to be invoked.
+		/// </summary>
+		public string MethodName { get; set; }
+
+		/// <summary>
+		/// Gets or sets method arguments (parameters).
+		/// </summary>
+		public object[] Arguments { get; set; }
+
+		/// <summary>
+		/// Returns a string representation of this event arguments.
+		/// </summary>
+		/// <returns>String representation of data</returns>
+		public override string ToString()
+		{
+			var argsAsString = new List<string>();
+
+			if (Arguments != null)
+			{
+				foreach (object arg in Arguments)
+				{
+					if (arg == null)
+						argsAsString.Add("null");
+					else
+						argsAsString.Add(arg.ToString());
+				}
+			}
+
+			string argChain = string.Join(", ", argsAsString.ToArray());
+
+			return string.Format("{0}.{1}({2})", InterfaceName, MethodName, argChain);
+		}
 	}
 }
