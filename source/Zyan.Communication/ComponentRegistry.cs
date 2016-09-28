839aa95238b8f11609c660a64cdb6b280e23c2df
Alexey Yakovlev <yallie@yandex.ru>
2016-09-28 21:12:13 +0000

    [en] Extracted ComponentRegistry from ComponentCatalog class, added IComponentRegistry interface, issue #2602.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS15]$/cyan;C44675

---
+++ source/Zyan.Communication/ComponentRegistry.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System;
+
+namespace Zyan.Communication
+{
+	/// <summary>
+	/// The default implementation of the <see cref="IComponentRegistry"/> based on <see cref="ConcurrentDictionary{TKey, TValue}"/>.
+	/// </summary>
+	/// <seealso cref="Zyan.Communication.IComponentRegistry" />
+	public class ComponentRegistry : IComponentRegistry
+	{
+		private ConcurrentDictionary<string, ComponentRegistration> components = new ConcurrentDictionary<string, ComponentRegistration>();
+
+		/// <summary>
+		/// Gets the <see cref="ComponentRegistration"/> with the specified component name.
+		/// </summary>
+		/// <param name="componentName">Name of the component.</param>
+		public ComponentRegistration this[string componentName]
+		{
+			get
+			{
+				return components[componentName];
+			}
+		}
+
+		/// <summary>
+		/// Clears the list of registrations.
+		/// </summary>
+		public void Clear()
+		{
+			components.Clear();
+		}
+
+		/// <summary>
+		/// Gets all component registrations.
+		/// </summary>
+		public IEnumerable<ComponentRegistration> GetRegistrations()
+		{
+			return components.Values;
+		}
+
+		/// <summary>
+		/// Determines whether the specified component is registered.
+		/// </summary>
+		/// <param name="componentName">The name of the component.</param>
+		public bool IsRegistered(string componentName)
+		{
+			return components.ContainsKey(componentName);
+		}
+
+		/// <summary>
+		/// Registers the specified component name.
+		/// </summary>
+		/// <param name="componentName">Name of the component.</param>
+		/// <param name="registration">The registration.</param>
+		/// <param name="ignoreUpdates">If set to true, then duplicate registrations are ignored rather than updated.</param>
+		public void Register(string componentName, ComponentRegistration registration, bool ignoreUpdates)
+		{
+			if (ignoreUpdates)
+			{
+				components.TryAdd(componentName, registration);
+				return;
+			}
+
+			components[componentName] = registration;
+		}
+
+		/// <summary>
+		/// Removes the specified component.
+		/// </summary>
+		/// <param name="componentName">Name of the component.</param>
+		public void Remove(string componentName)
+		{
+			ComponentRegistration _;
+			components.TryRemove(componentName, out _);
+		}
+
+		/// <summary>
+		/// Tries to get the component registration.
+		/// </summary>
+		/// <param name="componentName">Name of the component.</param>
+		/// <param name="registration">The registration.</param>
+		public bool TryGetRegistration(string componentName, out ComponentRegistration registration)
+		{
+			return components.TryGetValue(componentName, out registration);
+		}
+	}
+}
---
+++ source/Zyan.Communication/IComponentRegistry.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace Zyan.Communication
+{
+	/// <summary>
+	/// Simple dictionary to handle registrations for the <see cref="ComponentCatalog"/>.
+	/// </summary>
+	public interface IComponentRegistry
+	{
+		/// <summary>
+		/// Determines whether the specified component is registered.
+		/// </summary>
+		/// <param name="componentName">The name of the component.</param>
+		bool IsRegistered(string componentName);
+
+		/// <summary>
+		/// Registers the specified component name.
+		/// </summary>
+		/// <param name="componentName">Name of the component.</param>
+		/// <param name="registration">The registration.</param>
+		/// <param name="ignoreUpdates">If set to true, then duplicate registrations are ignored rather than updated.</param>
+		void Register(string componentName, ComponentRegistration registration, bool ignoreUpdates);
+
+		/// <summary>
+		/// Removes the specified component name.
+		/// </summary>
+		/// <param name="componentName">Name of the component.</param>
+		void Remove(string componentName);
+
+		/// <summary>
+		/// Gets the <see cref="ComponentRegistration"/> with the specified component name.
+		/// </summary>
+		/// <param name="componentName">Name of the component.</param>
+		ComponentRegistration this[string componentName] { get; }
+
+		/// <summary>
+		/// Tries to get the component registration.
+		/// </summary>
+		/// <param name="componentName">Name of the component.</param>
+		/// <param name="registration">The registration.</param>
+		bool TryGetRegistration(string componentName, out ComponentRegistration registration);
+
+		/// <summary>
+		/// Gets all component registrations.
+		/// </summary>
+		IEnumerable<ComponentRegistration> GetRegistrations();
+
+		/// <summary>
+		/// Clears the list of registrations.
+		/// </summary>
+		void Clear();
+	}
+}
