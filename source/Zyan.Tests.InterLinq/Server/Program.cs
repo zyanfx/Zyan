2ddd3db653e932bb5c2e6370f55a0688c45b53c8
yallie <yallie@yandex.ru>
2017-11-02 03:20:25 +0300

    Implemented the InterLinq unit test server. Starting the server before running the unit tests and stopping it afterwards. Finally added the InterLinq unit tests to the test script.

--- source/Zyan.Tests.InterLinq/Server/Program.cs
+++ source/Zyan.Tests.InterLinq/Server/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using InterLinq.UnitTests.Server;
 using System.Threading;
+using Topshelf;
 
 namespace InterLinq.UnitTests
 {
@@ -8,27 +9,55 @@ namespace InterLinq.UnitTests
 	{
 		public static void Main()
 		{
-			Console.WriteLine("Starting server");
+			HostFactory.Run(x =>
+			{
+				Console.WriteLine("Setting up the InterLinq server");
+				x.Service<Service>(s =>
+				{
+					s.ConstructUsing(() => new Service());
+					s.WhenStarted(ts => ts.Start());
+					s.WhenStopped(ts => ts.Stop());
+				});
 
-			//TestServerNHibernate testServerWcfNHibernate = new TestServerNHibernate();
-			//testServerWcfNHibernate.Start();
-			//testServerWcfNHibernate.Publish();
-
-			//TestServerSql testServerWcfSql = new TestServerSql();
-			//testServerWcfSql.Start();
-			//testServerWcfSql.Publish();
-
-			TestServerObjects testServerObjects = new TestServerObjects();
-			testServerObjects.Start();
-			testServerObjects.Publish();
-
-			//TestServerEntityFramework4 testServerEntityFramework4 = new TestServerEntityFramework4();
-			//testServerEntityFramework4.Start();
-			//testServerEntityFramework4.Publish();
-
-			Console.WriteLine("Server is up and running...");
-			Console.ReadLine();
+				// these options can be overridden via the command line:
+				// Server install --localservice -servicename:InterLinq -description:Demo -displayname:InterLinqDemo
+				x.RunAsNetworkService();
+				x.SetDescription("InterLinq unit testing service");
+				x.SetDisplayName("InterLinqService");
+				x.SetServiceName("InterLinqService");
+				x.EnableServiceRecovery(rc => rc.RestartService(1)); // restart after one minute
+				x.StartAutomatically();
+			});
 		}
 
+		private class Service
+		{
+			public void Start()
+			{
+				//TestServerNHibernate testServerWcfNHibernate = new TestServerNHibernate();
+				//testServerWcfNHibernate.Start();
+				//testServerWcfNHibernate.Publish();
+
+				//TestServerSql testServerWcfSql = new TestServerSql();
+				//testServerWcfSql.Start();
+				//testServerWcfSql.Publish();
+
+				TestServerObjects testServerObjects = new TestServerObjects();
+				testServerObjects.Start();
+				testServerObjects.Publish();
+
+				//TestServerEntityFramework4 testServerEntityFramework4 = new TestServerEntityFramework4();
+				//testServerEntityFramework4.Start();
+				//testServerEntityFramework4.Publish();
+
+				//Console.WriteLine("Server is up and running...");
+				//Console.ReadLine();
+			}
+
+			public void Stop()
+			{
+				// TODO: dispose of all services
+			}
+		}
 	}
 }
