d2194e3c033d533eea5e792462b9609bbc910ef2
yallie <yallie@yandex.ru>
2023-04-20 02:32:24 +0300

    Unified the punctuation of the resource messages as suggested by ResX Manager.

--- source/Zyan.Communication/LanguageResource.Designer.cs
+++ source/Zyan.Communication/LanguageResource.Designer.cs
@@ -19,7 +19,7 @@ namespace Zyan.Communication {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class LanguageResource {
@@ -412,7 +412,7 @@ namespace Zyan.Communication {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Invalid format for &apos;address&apos; parameter: {0}.
+        ///   Looks up a localized string similar to Invalid format for &apos;address&apos; parameter: {0}..
         /// </summary>
         internal static string Format_Exception_InvalidAddressFormat {
             get {
